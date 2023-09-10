aa6a6f042cb95b96fa573371cd4534f90b3d5bf3
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-09-10 03:18:39 -0700

    renamed some properties

--- Program.cs
+++ Program.cs
@@ -9,22 +9,22 @@ app.MapGet("/items", async (TodoDB db) =>
     await db.Todos.ToListAsync());
 
 app.MapGet("/items/complete", async (TodoDB db) => 
-    await db.Todos.Where(t => t.isComplete).ToListAsync());
+    await db.Todos.Where(t => t.IsComplete).ToListAsync());
 
-app.MapGet("/items/{id}", async (int id, TodoDb db) =>
+app.MapGet("/items/{id}", async (int id, TodoDB db) =>
     await db.Todos.FindAsync(id)
         is Todo todo
             ? Results.Ok(todo)
             : Results.NotFound());
 
-app.MapPost("/items", async (Todo todo, TodoDb db) => {
+app.MapPost("/items", async (Todo todo, TodoDB db) => {
     db.Todos.Add(todo);
     await db.SaveChangesAsync();
 
-    return Results.Created($"/todoitems/{todo.Id}", todo);
+    return Results.Created($"/todoitems/{todo.ID}", todo);
 });
 
-app.MapPut("/items/{id}", async (int id, Todo inputTodo, TodoDb db) => {
+app.MapPut("/items/{id}", async (int id, Todo inputTodo, TodoDB db) => {
     var todo = await db.Todos.FindAsync(id);
 
     if (todo is null) return Results.NotFound();
@@ -37,7 +37,7 @@ app.MapPut("/items/{id}", async (int id, Todo inputTodo, TodoDb db) => {
     return Results.NoContent();
 });
 
-app.MapDelete("/items/{id}", async (int id, TodoDb db) => {
+app.MapDelete("/items/{id}", async (int id, TodoDB db) => {
     if (await db.Todos.FindAsync(id) is Todo todo) {
         db.Todos.Remove(todo);
         await db.SaveChangesAsync();
@@ -46,3 +46,5 @@ app.MapDelete("/items/{id}", async (int id, TodoDb db) => {
 
     return Results.NotFound();
 });
+
+app.Run();

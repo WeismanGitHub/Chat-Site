16e81f9d5310305b630350f7d4e5c6778a0e98f6
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-09-10 03:10:09 -0700

    created api

--- Program.cs
+++ Program.cs
@@ -1,6 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TodoDB>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/items", async (TodoDB db) => 
+    await db.Todos.ToListAsync());
 
-app.Run();
+app.MapGet("/items/complete", async (TodoDB db) => 
+    await db.Todos.Where(t => t.isComplete).ToListAsync());
+
+app.MapGet("/items/{id}", async (int id, TodoDb db) =>
+    await db.Todos.FindAsync(id)
+        is Todo todo
+            ? Results.Ok(todo)
+            : Results.NotFound());
+
+app.MapPost("/items", async (Todo todo, TodoDb db) => {
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/todoitems/{todo.Id}", todo);
+});
+
+app.MapPut("/items/{id}", async (int id, Todo inputTodo, TodoDb db) => {
+    var todo = await db.Todos.FindAsync(id);
+
+    if (todo is null) return Results.NotFound();
+
+    todo.Name = inputTodo.Name;
+    todo.IsComplete = inputTodo.IsComplete;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/items/{id}", async (int id, TodoDb db) => {
+    if (await db.Todos.FindAsync(id) is Todo todo) {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+
+    return Results.NotFound();
+});

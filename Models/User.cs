70f69ca3f2f0855fdfabca7d2cb644dd4caf895f
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-09-11 01:54:10 -0700

    deleted everything

--- Models/User.cs
+++
@@ -1,7 +0,0 @@
-namespace ChatAPI.Models;
-
-public class User {
-    public string ID { get; set; }
-    public string Name { get; set; }
-    public string Password { get; set; } // I know I need to hash it.
-}

cc2d303f59498a1e044d71be0496783dc296f268
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-09-12 16:29:34 -0700

    converted to typescript

--- reactapp/.eslintrc.cjs
+++ reactapp/.eslintrc.cjs
@@ -5,11 +5,10 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  parser: '@typescript-eslint/parser',  settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
---
+++ reactapp/src/main.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client'
+import App from './app.tsx'
+import React from 'react'
+import './css/index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+)

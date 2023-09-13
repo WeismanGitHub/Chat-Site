77f49afa27c353ffa3dd8c8093b85e4eb87652ea
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-09-13 03:19:49 -0700

    created User model

---
+++ API/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Models {
+    public class User {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}

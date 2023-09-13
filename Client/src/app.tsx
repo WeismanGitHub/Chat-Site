026e88db3ae448261d403b030f497c400032b62e
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-09-13 08:59:19 -0700

    implemented react-query

--- Client/src/app.tsx
+++ Client/src/app.tsx
@@ -1,64 +1,17 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query'
+import ky from 'ky';
 
 export default function App() {
+    const { isLoading, error, data } = useQuery({
+        queryKey: ['data'],
+        queryFn: () => ky.get('https://localhost:7005/').text(),
+    })
+
+    if (isLoading) return 'Loading...'
+
+    if (error) return 'An error has occurred: ' + (error instanceof Error ? error.message : 'unknown')
+  
     return (<>
-        hello world
+        {data}
     </>)
-}
-export default class App extends Component {
-    static displayName = App.name;
-
-    constructor(props) {
-        super(props);
-        this.state = { forecasts: [], loading: true };
-    }
-
-    componentDidMount() {
-        this.populateWeatherData();
-    }
-
-    static renderForecastsTable(forecasts) {
-        return (
-            <table className='table table-striped' aria-labelledby="tabelLabel">
-                <thead>
-                    <tr>
-                        <th>Date</th>
-                        <th>Temp. (C)</th>
-                        <th>Temp. (F)</th>
-                        <th>Summary</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {forecasts.map(forecast =>
-                        <tr key={forecast.date}>
-                            <td>{forecast.date}</td>
-                            <td>{forecast.temperatureC}</td>
-                            <td>{forecast.temperatureF}</td>
-                            <td>{forecast.summary}</td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
-        );
-    }
-
-    render() {
-        let contents = this.state.loading
-            ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-            : App.renderForecastsTable(this.state.forecasts);
-
-        return (
-            <div>
-                <h1 id="tabelLabel" >Weather forecast</h1>
-                <p>This component demonstrates fetching data from the server.</p>
-                {contents}
-            </div>
-        );
-    }
-
-    async populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        const data = await response.json();
-        this.setState({ forecasts: data, loading: false });
-    }
 }

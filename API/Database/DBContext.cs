e6eb62a1ed486c0bd828a8a7e83785732a315b24
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-09-14 05:39:05 -0700

    created DBContext

---
+++ API/Database/DBContext.cs
@@ -0,0 +1,17 @@
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity;
+using API.Models;
+
+namespace API.DAL {
+    public class UserContext : DbContext {
+
+        public UserContext() : base("UserContext") {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}

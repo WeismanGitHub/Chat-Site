57b670199a5cab8a1cf7c79b91f78cd6dea602d7
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-09-15 13:13:51 -0700

    got dbcontext functional

--- API/Database/DBContext.cs
+++ API/Database/DBContext.cs
@@ -3,14 +3,15 @@ using API.Models;
 
 namespace API.Database {
     public class DataContext : DbContext {
-        protected readonly IConfiguration Configuration;
-
-        public DataContext(IConfiguration configuration) {
-            Configuration = configuration;
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder options) {
-            options.UseSqlite(Configuration.GetConnectionString("SQLiteDatabase"));
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            if (!optionsBuilder.IsConfigured) {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+                var connectionString = configuration.GetConnectionString("SQLiteDatabase");
+                optionsBuilder.UseSqlite(connectionString);
+            }
         }
 
         public DbSet<User> Users { get; set; }

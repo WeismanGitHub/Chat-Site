4504aa88148f56430f62d84de066a91279206777
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-09-17 08:40:34 -0700

    replaced database context

---
+++ API/Database/APIContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using API.Models;
+
+namespace API.Database {
+    public class APIContext : DbContext {
+        public APIContext(DbContextOptions<APIContext> options) : base(options) {
+        }
+
+        public DbSet<User> User { get; set; } = default!;
+    }
+}

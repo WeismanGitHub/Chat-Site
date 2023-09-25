c2c4297fe8280120c9ca2a0596f0eac8acd049ac
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-09-25 15:06:03 -0700

    created conversation model

---
+++ Library/Models/Conversation.cs
@@ -0,0 +1,8 @@
+﻿namespace Library.Models;
+public class Conversation {
+    [BsonId, BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public HashSet<string> MemberIds { get; set; } = new();
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}

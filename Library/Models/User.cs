71240b6dfcfc155c47f1377cbb790614311ec976
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-09-25 15:06:16 -0700

    added necessary fields

--- Library/Models/User.cs
+++ Library/Models/User.cs
@@ -1,3 +1,15 @@
 ﻿namespace Library.Models;
 public class User {
+    [BsonId, BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    public string ObjectIdentifier { get; set; }
+    public string DisplayName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public HashSet<string> FriendIds { get; set; } = new();
+    public HashSet<string> ConversationIds { get; set; } = new();
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    //public List<User> GetFriends() {
+    //}
 }

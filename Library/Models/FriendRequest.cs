f059f7388315c5b11a703adac322f86988fc3caf
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-09-25 13:53:44 -0700

    created FriendRequest model

---
+++ Library/Models/FriendRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Library.Models;
+public class FriendRequest {
+    [BsonId, BsonRepresentation(BsonType.ObjectId)]
+    public string SenderId { get; set; }
+    [BsonId, BsonRepresentation(BsonType.ObjectId)]
+    public string ReceiverId { get; set; }
+    public string Message { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}

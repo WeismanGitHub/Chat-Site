00df275189b33d1d17556f29ffb223275e6c57f2
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-09-25 23:06:08 -0700

    created

---
+++ Library/DataAccess/MongoFriendRequestData.cs
@@ -0,0 +1,17 @@
+﻿namespace Library.DataAccess; 
+public class MongoFriendRequestData {
+    private readonly IMongoCollection<FriendRequest> _friendRequests;
+    public MongoFriendRequestData(DbConnection db) {
+        _friendRequests = db.FriendRequestCollection;
+    }
+
+    public void AcceptFriendRequest(FriendRequest friendRequest) {
+
+    }
+    public void DeclineFriendRequest(FriendRequest friendRequest) {
+
+    }
+    public void DeleteFriendRequest(FriendRequest friendRequest) {
+
+    }
+}

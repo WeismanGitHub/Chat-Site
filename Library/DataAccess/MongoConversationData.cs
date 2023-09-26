ee37f9d80ee46e8a1fd74b8067ebd0250b52ea65
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-09-25 23:30:20 -0700

    created

---
+++ Library/DataAccess/MongoConversationData.cs
@@ -0,0 +1,15 @@
+﻿namespace Library.DataAccess; 
+public class MongoConversationData {
+    private readonly IMongoCollection<Conversation> _conversations;
+    public MongoConversationData(IDbConnection db) {
+        _conversations = db.ConversationCollection;
+    }
+    public Conversation CreateConversation(HashSet<string> memberIds, string Name) {
+        var convo = new Conversation {
+            MemberIds = memberIds,
+            Name = Name
+        };
+
+        return convo;
+    }
+}

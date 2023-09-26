a9377927a98be426b6aca86198da59d7737ec799
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-09-25 21:01:22 -0700

    added more methods

--- Library/DataAccess/MongoUserData.cs
+++ Library/DataAccess/MongoUserData.cs
@@ -1,7 +1,7 @@
 ﻿using ZstdSharp.Unsafe;
 
 namespace Library.DataAccess;
-public class MongoUserData {
+public class MongoUserData : IUserData {
     private readonly IMongoCollection<User> _users;
 
     public MongoUserData(IDbConnection db) {
@@ -19,9 +19,20 @@ public class MongoUserData {
 
         return user.FirstOrDefault();
     }
-    public async Task<User> GetUserFromAuthentication(string id) {
-        var user = await _users.FindAsync(user => user.Id == id);
+    public async Task<User> GetUserFromAuthentication(string objectId) {
+        var user = await _users.FindAsync(user => user.ObjectIdentifier == objectId);
 
         return user.FirstOrDefault();
+    }
+
+    public Task CreateUser(User user) {
+        return _users.InsertOneAsync(user);
+    }
+
+    public Task UpdateUser(User user) {
+        var filter = Builders<User>.Filter.Eq("Id", user.Id);
+        var options = new ReplaceOptions { IsUpsert = true };
+
+        return _users.ReplaceOneAsync(filter, user, options);
     }
 }

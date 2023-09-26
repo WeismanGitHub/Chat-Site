291f72902e304a221145dce7d8afbf3787da0648
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-09-25 21:01:29 -0700

    created interface

---
+++ Library/DataAccess/IUserData.cs
@@ -0,0 +1,9 @@
+﻿namespace Library.DataAccess {
+    public interface IUserData {
+        Task CreateUser(User user);
+        Task<User> GetUser(string id);
+        Task<User> GetUserFromAuthentication(string objectId);
+        Task<List<User>> GetUsers();
+        Task UpdateUser(User user);
+    }
+}

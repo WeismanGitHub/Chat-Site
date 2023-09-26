1708729dcd687f3c8288d90e0185515ebd43c1d0
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-09-25 20:35:33 -0700

    created and added methods for users

---
+++ Library/DataAccess/MongoUserData.cs
@@ -0,0 +1,27 @@
+﻿using ZstdSharp.Unsafe;
+
+namespace Library.DataAccess;
+public class MongoUserData {
+    private readonly IMongoCollection<User> _users;
+
+    public MongoUserData(IDbConnection db) {
+        _users = db.UserCollection;
+    }
+
+    public async Task<List<User>> GetUsers() {
+        var results = await _users.FindAsync(_ => true);
+
+        return results.ToList();
+    }
+
+    public async Task<User> GetUser(string id) {
+        var user = await _users.FindAsync(user => user.Id == id);
+
+        return user.FirstOrDefault();
+    }
+    public async Task<User> GetUserFromAuthentication(string id) {
+        var user = await _users.FindAsync(user => user.Id == id);
+
+        return user.FirstOrDefault();
+    }
+}

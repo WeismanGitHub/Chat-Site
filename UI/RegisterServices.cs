af6c4b1c677cae420e12f718a552c1995426d7da
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-09-25 21:38:02 -0700

    uninstalled caching package

--- UI/RegisterServices.cs
+++ UI/RegisterServices.cs
@@ -2,7 +2,6 @@
 public static class RegisterServices {
     public static void ConfigureServices(this WebApplicationBuilder builder) {
         builder.Services.AddServerSideBlazor();
-        builder.Services.AddMemoryCache();
         builder.Services.AddRazorPages();
     }
 }

1dbd59deb2203edfc2d6aed6e7b9c8895336cb3b
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-09-25 19:58:47 -0700

    added all necessary stuff for dbconnection to work

--- Library/DataAccess/DbConnection.cs
+++ Library/DataAccess/DbConnection.cs
@@ -1,3 +1,30 @@
-﻿namespace Library.DataAccess; 
-public class DbConnection {
+﻿using Microsoft.Extensions.Configuration;
+
+using MongoDB.Driver;
+
+namespace Library.DataAccess;
+public class DbConnection : IDbConnection {
+    private string _connectionId = "MongoDB";
+    private readonly IConfiguration _config;
+    private readonly IMongoDatabase _db;
+
+    public string DbName { get; private set; }
+    public string FriendRequestsCollectionName { get; private set; } = "FriendRequests";
+    public string ConversationCollectionName { get; private set; } = "Conversations";
+    public string UserCollectionName { get; private set; } = "Users";
+
+    public MongoClient Client { get; private set; }
+    public IMongoCollection<FriendRequest> FriendRequestCollection { get; private set; }
+    public IMongoCollection<Conversation> ConversationCollection { get; private set; }
+    public IMongoCollection<User> UserCollection { get; private set; }
+    public DbConnection(IConfiguration config) {
+        _config = config;
+        Client = new MongoClient(_config.GetConnectionString(_connectionId));
+        DbName = _config["DatabaseName"]!;
+        _db = Client.GetDatabase(DbName);
+
+        FriendRequestCollection = _db.GetCollection<FriendRequest>(FriendRequestsCollectionName);
+        ConversationCollection = _db.GetCollection<Conversation>(ConversationCollectionName);
+        UserCollection = _db.GetCollection<User>(UserCollectionName);
+    }
 }

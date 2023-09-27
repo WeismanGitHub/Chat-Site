36e0b40e179c5c9aee00e9bee372938a294d18f4
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-09-26 18:27:52 -0700

    added database stuff

--- UI/RegisterServices.cs
+++ UI/RegisterServices.cs
@@ -3,5 +3,10 @@ public static class RegisterServices {
     public static void ConfigureServices(this WebApplicationBuilder builder) {
         builder.Services.AddServerSideBlazor();
         builder.Services.AddRazorPages();
+
+        builder.Services.AddSingleton<IDbConnection, DbConnection>();
+        builder.Services.AddSingleton<IFriendRequestData, MongoFriendRequestData>();
+        builder.Services.AddSingleton<IConversationData, MongoConversationData>();
+        builder.Services.AddSingleton<IUserData, MongoUserData>();
     }
 }

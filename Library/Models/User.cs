d4d463fe3196e5c49a2e0d35a0bc1ba41d09ac58
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-05 14:58:38 -0700

    added a bit of validation

--- Library/Models/User.cs
+++ Library/Models/User.cs
@@ -1,4 +1,6 @@
-﻿namespace Library.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models;
 public class UserModel {
     [BsonId, BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; }
@@ -7,7 +9,9 @@ public class UserModel {
     public string DisplayName { get; set; }
     [BsonRequired]
     public string Email { get; set; }
+    [MaxLength(100, ErrorMessage = "Cannot add more than 100 friends.")]
     public HashSet<string> FriendIds { get; set; } = new();
+    [MaxLength(100, ErrorMessage = "Cannot join more than 100 conversations.")]
     public HashSet<string> ConversationIds { get; set; } = new();
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }

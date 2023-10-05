214d2be16f2e00c780c5ca941a4b2cae8ebf9b52
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-05 02:50:12 -0700

    reformatted

--- UI/Program.cs
+++ UI/Program.cs
@@ -1,22 +1,17 @@
 using Microsoft.AspNetCore.Rewrite;
-
 using UI;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.ConfigureServices();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment()) { 
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
 app.UseAuthentication();
 
@@ -25,7 +20,7 @@ app.UseRewriter(new RewriteOptions().Add(
         if (context.HttpContext.Request.Path == "/MicrosoftIdentity/Account/SignedOut") {
             context.HttpContext.Response.Redirect("/");
         }
-    }    
+    }
 ));
 
 app.MapControllers();

9c63c88fb81838172b75ce3c4965153ba5cbff13
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-06 04:51:38 -0700

    created fake user model

--- Library/DataGenerator.cs
+++ Library/DataGenerator.cs
@@ -2,8 +2,14 @@
 
 namespace Library;
 public class DataGenerator {
+    private Faker<UserModel> fakeUserModel;
     public DataGenerator(int seed) {
         Randomizer.Seed = new Random(seed);
+
+        fakeUserModel = new Faker<UserModel>()
+            .RuleFor(u => u.ObjectIdentifier, f => Guid.NewGuid().ToString())
+            .RuleFor(u => u.Email, f => f.Internet.Email())
+            .RuleFor(u => u.DisplayName, f => f.Internet.UserName());
     }
 
     public async Task<bool> CollectionsAreEmpty() {

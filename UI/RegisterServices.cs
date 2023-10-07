aa7d5d6a7d61ca3a6b8364918445fce57a672c3c
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-07 14:23:56 -0700

    use passed in connectionId

--- UI/RegisterServices.cs
+++ UI/RegisterServices.cs
@@ -4,16 +4,13 @@ using Microsoft.Identity.Web;
 
 namespace UI;
 public static class RegisterServices {
-    public static void ConfigureServices(this WebApplicationBuilder builder) {
+    public static void ConfigureServices(this WebApplicationBuilder builder, string connectionId) {
         builder.Services.AddServerSideBlazor();
         builder.Services.AddRazorPages();
         builder.Services.AddControllersWithViews().AddMicrosoftIdentityUI();
 
         builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
             .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureADB2C"));
-
-        string env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-        string connectionId = env == "Development" ? "MongoDev" : "MongoProd";
 
         builder.Services.AddSingleton<IDbConnection, DbConnection>(_ => new DbConnection(builder.Configuration, connectionId));
         builder.Services.AddSingleton<IFriendRequestData, MongoFriendRequestData>();

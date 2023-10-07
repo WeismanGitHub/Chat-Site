1e86260b6f761e7361c594bff752fdd01b11eacf
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-07 13:42:13 -0700

    removed unnecessary using statement

--- UI/RegisterServices.cs
+++ UI/RegisterServices.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Identity.Web.UI;
 using Microsoft.Identity.Web;
-using System.Drawing;
 
 namespace UI;
 public static class RegisterServices {

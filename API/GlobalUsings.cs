0dd618b7f0b10cd57e34ac5cc52a19dadef9d8cd
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-08 10:05:11 -0700

    added using statements

--- API/GlobalUsings.cs
+++ API/GlobalUsings.cs
@@ -1,5 +1,8 @@
 ﻿global using MongoDB.Bson.Serialization.Attributes;
+global using FluentValidation;
 global using MongoDB.Entities;
 global using Database.Models;
 global using MongoDB.Driver;
 global using MongoDB.Bson;
+global using FastEndpoints;
+global using API;

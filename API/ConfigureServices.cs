643a9a5c459fe24265d3ba23fb35c71f90fb3861
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-08 10:04:48 -0700

    updated

---
+++ API/ConfigureServices.cs
@@ -0,0 +1,19 @@
+﻿using FastEndpoints;
+using FastEndpoints.Swagger;
+
+namespace API;
+
+public static class ConfigureServices {
+    public static void Configure(IServiceCollection services) {
+        services.AddFastEndpoints()
+            .SwaggerDocument()
+            .AddResponseCaching();
+
+        services.AddCors(options => {
+            options.AddPolicy(name: "_myAllowSpecificOrigins", policy => {
+                policy.WithOrigins("https://localhost:5173");
+            });
+        });
+
+    }
+}
---
+++ API/Settings.cs
@@ -0,0 +1,16 @@
+﻿namespace API;
+
+internal sealed class Settings {
+    public DatabaseSettings Database { get; set; }
+    public JWTAuthSettings Auth { get; set; }
+
+    public class DatabaseSettings {
+        public string Name { get; set; }
+        public string ConnectionString { get; set; }
+    }
+
+    public class JWTAuthSettings {
+        public int TokenValidityMinutes { get; set; }
+        public string SigningKey { get; set; }
+    }
+}

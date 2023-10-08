1632cc68839af9d633ebd7f5f56add039ebe1c4d
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-08 10:05:30 -0700

    created

---
+++ API/Database/DataGenerator.cs
@@ -0,0 +1,30 @@
+﻿//using Bogus;
+
+//using System.Data;
+
+//namespace Library;
+//public class DataGenerator {
+//    public DataGenerator(IDbConnection db, int seed) {
+//        Randomizer.Seed = new Random(seed);
+//    }
+
+//    public async void PopulateWithBogus() {
+//        if ((await userData.GetAll()).Count > 0) return;
+
+//        var users = await CreateUsers();
+
+//        foreach (var user in users) {
+//        }
+//    }
+
+//    public async Task<List<UserModel>> CreateUsers() {
+//        List<UserModel> users = new();
+
+//        foreach (var fakeUser in userData.faker.GenerateForever().Take(25)) {
+//            await userData.CreateUser(fakeUser);
+//            users.Add(fakeUser);
+//        }
+
+//        return users;
+//    }
+//}

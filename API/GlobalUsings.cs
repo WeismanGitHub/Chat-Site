96f8a1b71c6a55c967ee81097204793ca0dd6e17
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-09 15:09:25 -0700

    changed models to entitites

--- API/GlobalUsings.cs
+++ API/GlobalUsings.cs
@@ -1,7 +1,7 @@
 ﻿global using MongoDB.Bson.Serialization.Attributes;
 global using FluentValidation;
 global using MongoDB.Entities;
-global using Database.Models;
+global using Database.Entities;
 global using MongoDB.Driver;
 global using MongoDB.Bson;
 global using FastEndpoints;

405202c0f92d78ef939340d8e5162d95e3c819e5
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-09 16:57:07 -0700

    added necessary methods

--- API/Features/Users/Signup/Data.cs
+++ API/Features/Users/Signup/Data.cs
@@ -1,5 +1,14 @@
 ﻿namespace API.Features.Users.Signup;
 
-internal static class Data {
+public static class Data {
+    internal static Task<bool> EmailAddressIsTaken(string email) {
+        return DB
+            .Find<User>()
+            .Match(a => a.Email == email)
+            .ExecuteAnyAsync();
+    }
 
+    internal static Task CreateNewUser(User user) {
+        return user.SaveAsync();
+    }
 }

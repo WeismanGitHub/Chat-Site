c2ad617cf6cdf511d2c22ad4f6baa33ba5a72085
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-09 16:57:40 -0700

    created mapper

--- API/Features/Users/Signup/Mapper.cs
+++ API/Features/Users/Signup/Mapper.cs
@@ -1,5 +1,9 @@
 ﻿namespace API.Features.Users.Signup;
 
-internal sealed class Mapper : Mapper<Request, Response, object> {
-
+internal sealed class Mapper : Mapper<Request, Response, User> {
+    public override User ToEntity(Request req) => new() {
+        Email = req.Email.ToLower(),
+        DisplayName = req.DisplayName,
+        PasswordHash = BCrypt.Net.BCrypt.HashPassword(req.Password)
+    };
 }

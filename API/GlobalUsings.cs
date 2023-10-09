1310150544e2d3fda23f12c5e934147229d616f1
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-09 15:54:36 -0700

    changed global usings

--- API/GlobalUsings.cs
+++ API/GlobalUsings.cs
@@ -1,8 +1,8 @@
 ﻿global using MongoDB.Bson.Serialization.Attributes;
+global using Database.Entities;
 global using FluentValidation;
 global using MongoDB.Entities;
-global using Database.Entities;
 global using MongoDB.Driver;
-global using MongoDB.Bson;
 global using FastEndpoints;
+global using MongoDB.Bson;
 global using API;

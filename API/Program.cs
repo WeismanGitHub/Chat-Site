823eb29c1abfc5504fb8d4a8780c25811a2c86eb
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-09 15:09:51 -0700

    works

--- API/Program.cs
+++ API/Program.cs
@@ -3,26 +3,26 @@ using MongoDB.Bson.Serialization;
 using FastEndpoints.Swagger;
 
 var builder = WebApplication.CreateBuilder();
+ConfigureServices.Configure(builder.Services);
 var config = builder.Configuration;
 
-string connectionId = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development" ? "MongoDev" : "MongoProd";
+string connectionId = "MongoProd";// Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development" ? "MongoDev" : "MongoProd";
 var settings = builder.Configuration.GetSection("Settings").Get<Settings>()!;
 settings.Database.ConnectionString = builder.Configuration.GetConnectionString(connectionId)!;
+settings.Auth.SigningKey = builder.Configuration["SigningKey"]!;
+builder.Services.AddAuthentication().AddJwtBearer(settings.Auth.SigningKey);
 
 var app = builder.Build();
 
-app.UseFastEndpoints()
-    .UseAuthentication()
+app.UseAuthentication()
     .UseHttpsRedirection()
     .UseAuthorization()
     .UseResponseCaching()
-    .UseFastEndpoints(c => c.Serializer.Options.PropertyNamingPolicy = null);
+    .UseSwaggerGen()
+    .UseFastEndpoints(c => c.Endpoints.RoutePrefix = "api");
 
 await InitDatabase();
 
-if (!builder.Environment.IsProduction()) {
-    app.UseSwaggerGen();
-}
 
 app.Run();
 
@@ -30,6 +30,6 @@ async Task InitDatabase() {
     BsonSerializer.RegisterSerializer(new ObjectSerializer(type =>
         ObjectSerializer.DefaultAllowedTypes(type) || type.Name!.EndsWith("Message"))
     );
-    await DB.InitAsync(settings.Database.ConnectionString);
+    await DB.InitAsync(settings.Database.Name, MongoClientSettings.FromConnectionString(settings.Database.ConnectionString));
     await DB.MigrateAsync();
 }

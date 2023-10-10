d6ece45c878108b51bc399fa5000087f47413401
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-10 09:32:25 -0700

    removed unnecessary method

--- API/Features/Users/Signup/Data.cs
+++ API/Features/Users/Signup/Data.cs
@@ -4,11 +4,7 @@ public static class Data {
     internal static Task<bool> EmailAddressIsTaken(string email) {
         return DB
             .Find<User>()
-            .Match(a => a.Email == email)
+            .Match(u => u.Email == email)
             .ExecuteAnyAsync();
-    }
-
-    internal static Task CreateNewUser(User user) {
-        return user.SaveAsync();
     }
 }

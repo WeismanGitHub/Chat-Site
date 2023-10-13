54755df2c1c9eb9b6d3e0f5118d40d4d26e30021
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-13 16:24:50 -0700

    created

---
+++ API/Endpoints/Users/Signin/Data.cs
@@ -0,0 +1,15 @@
+﻿namespace API.Endpoints.Users.Signin;
+
+public static class Data {
+    internal static Task<User?> GetAccount(string email) {
+        return DB
+            .Find<User>()
+            .Match(u => u.Email == email)
+            .Project(u => new() {
+                PasswordHash = u.PasswordHash,
+                DisplayName = u.DisplayName,
+                ID = u.ID,
+            })
+            .ExecuteSingleAsync();
+    }
+}

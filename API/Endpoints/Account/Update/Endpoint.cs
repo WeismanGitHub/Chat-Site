98b1d3d77cf9a0807da3404916dbcd1bde8bd331
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-16 20:37:58 -0700

    finished endpoint

---
+++ API/Endpoints/Account/Update/Endpoint.cs
@@ -0,0 +1,22 @@
+﻿namespace API.Endpoints.Account.Update;
+
+internal sealed class Endpoint : Endpoint<Request, Response, Mapper> {
+    public override void Configure() {
+        Patch("/Update");
+        Group<AccountGroup>();
+        Version(1);
+
+        Summary(settings => {
+            settings.Summary = "Update an account.";
+            settings.ExampleRequest = new Request {
+                DisplayName = "New Name",
+            };
+        });
+    }
+
+    public override async Task HandleAsync(Request req, CancellationToken cancellationToken) {
+        var account = Map.ToEntity(req);
+
+        await Data.Update(account);
+    }
+}

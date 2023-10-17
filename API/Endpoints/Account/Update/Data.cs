6675a85543de6d14324df88de158f04be65877bc
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-16 20:39:29 -0700

    finished

---
+++ API/Endpoints/Account/Update/Data.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Endpoints.Account.Update;
+
+public static class Data {
+    internal static Task<bool> Update(AccountData account) {
+        return DB
+            .Find<User>()
+            .Match(u => u.ID == account.ID)
+            .ExecuteAnyAsync();
+    }
+}
---
+++ API/Endpoints/Account/Update/Mapper.cs
@@ -0,0 +1,15 @@
+﻿namespace API.Endpoints.Account.Update;
+
+internal sealed class AccountData {
+    public string ID { get; set; }
+    public string DisplayName { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+}
+internal sealed class Mapper : Mapper<Request, Response, AccountData> {
+    public override AccountData ToEntity(Request req) => new() {
+        Email = req.Email.ToLower(),
+        DisplayName = req.DisplayName,
+        PasswordHash = BCrypt.Net.BCrypt.HashPassword(req.Password)
+    };
+}

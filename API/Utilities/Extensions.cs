16fdf0ff3de08c7139ebabea66102ca2cf835d47
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-17 19:21:35 -0700

    created extensions

---
+++ API/Utilities/Extensions.cs
@@ -0,0 +1,25 @@
+﻿namespace API;
+
+internal static class Extensions {
+    public static bool IsAValidPassword(this string password) {
+        if (password == null) return false;
+
+        bool meetsLengthRequirements = password.Length is >= User.MinPasswordLength and <= User.MaxPasswordLength;
+        bool hasUpperCaseLetter = false;
+        bool hasLowerCaseLetter = false;
+        bool hasDecimalDigit = false;
+
+        if (meetsLengthRequirements) {
+            foreach (char c in password) {
+                if (char.IsUpper(c)) hasUpperCaseLetter = true;
+                else if (char.IsLower(c)) hasLowerCaseLetter = true;
+                else if (char.IsDigit(c)) hasDecimalDigit = true;
+            }
+        }
+
+        return meetsLengthRequirements &&
+            hasUpperCaseLetter &&
+            hasLowerCaseLetter &&
+            hasDecimalDigit;
+    }
+}

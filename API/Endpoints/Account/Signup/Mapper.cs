971e988574d4c13099348a628ad0e6d687a08d7e
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-19 16:22:30 -0700

    deleted

--- API/Endpoints/Account/Signup/Mapper.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace API.Endpoints.Account.Signup;
-
-internal sealed class Mapper : Mapper<Request, Response, User> {
-    public override User ToEntity(Request req) => new() {
-        Email = req.Email.ToLower(),
-        DisplayName = req.DisplayName,
-        PasswordHash = BCrypt.Net.BCrypt.HashPassword(req.Password)
-    };
-}

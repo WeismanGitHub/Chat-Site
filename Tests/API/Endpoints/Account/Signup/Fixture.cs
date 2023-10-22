16465e2ba22d3cfb950b1edacbc45fc3cf88c731
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-21 20:09:27 -0700

    created

---
+++ Tests/API/Endpoints/Account/Signup/Fixture.cs
@@ -0,0 +1,20 @@
+﻿using API.Database.Entities;
+using MongoDB.Entities;
+
+namespace Tests.API.Endpoints.Account.Signup;
+
+public class Fixture : TestFixture<Program> {
+    public List<string> userIds = new();
+    public Fixture(IMessageSink sink) : base(sink) { }
+
+    protected override Task SetupAsync() {
+        return Task.CompletedTask;
+    }
+
+    protected override void ConfigureServices(IServiceCollection services) {
+    }
+
+    protected override async Task TearDownAsync() {
+        await DB.DeleteAsync<User>(userIds.ToArray());
+    }
+}

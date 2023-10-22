da0230fb90603267249817c75f13d6729876a09c
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-21 21:19:11 -0700

    wrote my first test

--- Tests/API/Endpoints/Account/Signup/Fixture.cs
+++ Tests/API/Endpoints/Account/Signup/Fixture.cs
@@ -4,10 +4,11 @@ using MongoDB.Entities;
 namespace Tests.API.Endpoints.Account.Signup;
 
 public class Fixture : TestFixture<Program> {
-    public List<string> userIds = new();
+    public List<string> userEmails = new();
     public Fixture(IMessageSink sink) : base(sink) { }
 
     protected override Task SetupAsync() {
+        Console.WriteLine("tfygh");
         return Task.CompletedTask;
     }
 
@@ -15,6 +16,6 @@ public class Fixture : TestFixture<Program> {
     }
 
     protected override async Task TearDownAsync() {
-        await DB.DeleteAsync<User>(userIds.ToArray());
+        //await DB.DeleteAsync<User>(u => userEmails.Contains(u.Email));
     }
 }

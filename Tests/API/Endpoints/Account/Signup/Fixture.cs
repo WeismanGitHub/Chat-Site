5a3915a8a6c7b51c59aea046df5f65e7c1fba93b
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-23 19:36:38 -0700

    removed AccountEmail property

--- Tests/API/Endpoints/Account/Signup/Fixture.cs
+++ Tests/API/Endpoints/Account/Signup/Fixture.cs
@@ -5,8 +5,6 @@ namespace Tests.API.Endpoints.Account.Signup;
 
 public class Fixture : TestFixture<Program> {
     public Fixture(IMessageSink sink) : base(sink) { }
-
-    public string AccountEmail { get; set; }
     public string Token { get; set; }
 
     protected override Task SetupAsync() {
@@ -17,6 +15,6 @@ public class Fixture : TestFixture<Program> {
     }
 
     protected override async Task TearDownAsync() {
-        await DB.DeleteAsync<User>(u => u.Email == AccountEmail);
+        await DB.DeleteAsync<User>(u => u.Email == ValidAccount.Email);
     }
 }

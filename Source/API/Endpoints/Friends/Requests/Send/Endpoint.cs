4754e4baeb2d3b27172c74654c40827fe733107b
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-24 22:10:43 -0700

    created

---
+++ Source/API/Endpoints/Friends/Requests/Send/Endpoint.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Options;
+
+using System.Security.Cryptography.Pkcs;
+
+namespace API.Endpoints.Friends.Requests.Send;
+
+public sealed class Endpoint : Endpoint<Request> {
+    public IOptions<Settings> Settings { get; set; } = null!;
+
+    public override void Configure() {
+        Post("/");
+        Group<RequestGroup>();
+        Version(1);
+        
+        Summary(settings => {
+            settings.Summary = "Send a friend request to a user.";
+        });
+    }
+
+    public override async Task HandleAsync(Request req, CancellationToken cancellationToken) {
+        var recipient = DB.Find<User>().OneAsync(req.AccountID);
+
+        if (req.RecipientID == req.AccountID) {
+            ThrowError("You cannot befriend yourself.", 400);
+        } else if (recipient == null) {
+            ThrowError("No users with that ID.", 400);
+        }
+
+        var friendReq = new FriendRequest() {
+            Message = req.Message,
+            RecipientId = req.RecipientID,
+            RequesterId = req.AccountID,
+        };
+
+        await DB.InsertAsync(friendReq);
+    }
+}

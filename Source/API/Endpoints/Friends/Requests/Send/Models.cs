f1c8bd41b7dd3249c16a912189bf80696f8f367c
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-24 23:57:46 -0700

    created validator

--- Source/API/Endpoints/Friends/Requests/Send/Models.cs
+++ Source/API/Endpoints/Friends/Requests/Send/Models.cs
@@ -7,3 +7,33 @@ public sealed class Request {
 
     public string? Message { get; set; }
 }
+
+internal sealed class Validator : Validator<Request> {
+    public Validator() {
+        this.CascadeMode = CascadeMode.StopOnFirstFailure;
+
+        RuleFor(req => req.RecipientID)
+            .NotEmpty()
+            .NotEqual(req => req.AccountID)
+            .WithMessage("You cannot befriend yourself.")
+            .Must(id => ObjectId.TryParse(id, out _))
+            .WithMessage("Invalid RecipientID.")
+            .MustAsync((id, _) => DB.Find<User>().MatchID(id).ExecuteAnyAsync())
+            .WithMessage("Recipient does not exist.");
+
+        RuleFor(req => req.Message)
+            .MinimumLength(1)
+            .MaximumLength(250);
+
+        RuleFor(req => req)
+            .Must(req => ObjectId.TryParse(req.RecipientID, out _))
+            .WithMessage("Invalid RecipientID.")
+            .MustAsync((req, _) =>
+                DB.Find<FriendRequest>()
+                     .Match(fr => fr.RecipientID == req.RecipientID)
+                     .Match(fr => fr.RequesterID == req.AccountID)
+                     .ExecuteAnyAsync()
+            )
+            .WithMessage("You've already sent this user a friend request.");
+    }
+}

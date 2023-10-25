6f2ce68451137943617b9f86d03a098ab540e98d
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-24 22:02:09 -0700

    deleted Friend class

--- Source/API/Endpoints/Friends/Get/Models.cs
+++ Source/API/Endpoints/Friends/Get/Models.cs
@@ -1,11 +1,5 @@
 ﻿namespace API.Endpoints.Friends.Get;
 
-public sealed class Friend {
-    public string ID { get; set; }
-    public string DisplayName { get; set; }
-    public DateTime CreatedAt { get; set; }
-}
-
 public sealed class Request {
     [From(Claim.AccountID, IsRequired = true)]
     public string AccountID { get; set; }

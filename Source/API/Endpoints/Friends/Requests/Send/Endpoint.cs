e761ea8681458ec9a3ab7ac374aed956e336aab8
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-24 23:58:02 -0700

    finished

--- Source/API/Endpoints/Friends/Requests/Send/Endpoint.cs
+++ Source/API/Endpoints/Friends/Requests/Send/Endpoint.cs
@@ -18,18 +18,10 @@ public sealed class Endpoint : Endpoint<Request> {
     }
 
     public override async Task HandleAsync(Request req, CancellationToken cancellationToken) {
-        var recipient = DB.Find<User>().OneAsync(req.AccountID);
-
-        if (req.RecipientID == req.AccountID) {
-            ThrowError("You cannot befriend yourself.", 400);
-        } else if (recipient == null) {
-            ThrowError("No users with that ID.", 400);
-        }
-
         var friendReq = new FriendRequest() {
             Message = req.Message,
-            RecipientId = req.RecipientID,
-            RequesterId = req.AccountID,
+            RecipientID = req.RecipientID,
+            RequesterID = req.AccountID,
         };
 
         await DB.InsertAsync(friendReq);

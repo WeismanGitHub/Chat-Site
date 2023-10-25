6912e52b847000ea75d44c2e6c3a6abb76bf857f
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-24 22:01:58 -0700

    created

---
+++ Source/API/Endpoints/Friends/Requests/Send/Models.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Endpoints.Friends.Requests.Send;
+
+public sealed class Request {
+    [From(Claim.AccountID, IsRequired = true)]
+    public string AccountID { get; set; }
+    public string RecipientID { get; set; }
+
+    public string? Message { get; set; }
+}

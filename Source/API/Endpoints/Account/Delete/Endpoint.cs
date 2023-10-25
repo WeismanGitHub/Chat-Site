33d83b8f59949f7bcfe3aaed6060b2d8403709aa
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-24 19:31:32 -0700

    implemented endpoint

---
+++ Source/API/Endpoints/Account/Delete/Endpoint.cs
@@ -0,0 +1,20 @@
+﻿using FastEndpoints.Security;
+
+namespace API.Endpoints.Account.Delete;
+
+sealed class Endpoint : Endpoint<Request> {
+    public override void Configure() {
+        Delete("/");
+        Group<AccountGroup>();
+        Version(1);
+
+        Summary(settings => {
+            settings.Summary = "Delete logged in account.";
+        });
+    }
+
+    public override async Task HandleAsync(Request req, CancellationToken cancellationToken) {
+        await Data.DeleteUser(req.AccountID);
+        await CookieAuth.SignOutAsync();
+    }
+}

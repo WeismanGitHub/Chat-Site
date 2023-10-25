56e1914be6f8f50f5b275bc6f39214c81059b34b
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-24 20:07:54 -0700

    ill finish once ive implemented more code

---
+++ Source/API/Endpoints/Account/Delete/Data.cs
@@ -0,0 +1,16 @@
+﻿namespace API.Endpoints.Account.Delete;
+
+public static class Data {
+    internal static async Task DeleteUser(User user) {
+        var conversations = await DB.Find<Conversation>().ManyAsync(convo => 
+            user.Conversations.Any(c => c.ID == convo.ID)
+        );
+
+        var transaction = new Transaction();
+        
+        await transaction.DeleteAsync<User>(user.ID);
+        //await transaction.Update<Conversation>().Match(convo => user.Conversations.)
+
+        await transaction.CommitAsync();
+    }
+}

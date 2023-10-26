f9902b7511408d41b0b18d0911940de2cddda386
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-25 22:14:33 -0700

    set up some validation

--- Source/API/Endpoints/Friends/Requests/Accept/Endpoint.cs
+++ Source/API/Endpoints/Friends/Requests/Accept/Endpoint.cs
@@ -13,6 +13,13 @@ public class Endpoint : Endpoint<Request> {
 
     public override async Task HandleAsync(Request req, CancellationToken cancellationToken) {
         var friendRequest = await DB.Find<FriendRequest>().MatchID(req.RequestID).ExecuteSingleAsync();
+
+        if (friendRequest == null) {
+            ThrowError("FriendRequest does not exist.", 404);
+        } else if (friendRequest.RecipientID != req.AccountID) {
+            ThrowError("You cannot accept this FriendRequest.", 403);
+        }
+
         Console.WriteLine(friendRequest.Message);
     }
 }

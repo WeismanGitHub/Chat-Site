d94d99d28d8abdcf5f0d76f580207bd897f185bf
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-25 21:59:32 -0700

    created

---
+++ Source/API/Endpoints/Friends/Requests/Accept/Endpoint.cs
@@ -0,0 +1,18 @@
+﻿namespace API.Endpoints.Friends.Requests.Accept;
+
+public class Endpoint : Endpoint<Request> {
+    public override void Configure() {
+        Post("/{RequestID}/accept");
+        Group<RequestGroup>();
+        Version(1);
+
+        Summary(settings => {
+            settings.Summary = "Accept a friend request.";
+        });
+    }
+
+    public override async Task HandleAsync(Request req, CancellationToken cancellationToken) {
+        var friendRequest = await DB.Find<FriendRequest>().MatchID(req.RequestID).ExecuteSingleAsync();
+        Console.WriteLine(friendRequest.Message);
+    }
+}

0700fcab3c248bd36817ecedcad2183bde7b71d7
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-27 22:32:03 -0700

    updated validator

--- Source/API/Endpoints/Friends/Requests/Send/Models.cs
+++ Source/API/Endpoints/Friends/Requests/Send/Models.cs
@@ -14,8 +14,6 @@ internal sealed class Validator : Validator<Request> {
 
         RuleFor(req => req.RecipientID)
             .NotEmpty()
-            .Equal(req => req.AccountID)
-            .WithMessage("You cannot befriend yourself.")
             .Must(id => ObjectId.TryParse(id, out _))
             .WithMessage("Invalid RecipientID.")
             .MustAsync((id, _) => DB.Find<User>().MatchID(id).ExecuteAnyAsync())
@@ -26,6 +24,8 @@ internal sealed class Validator : Validator<Request> {
             .MaximumLength(250);
 
         RuleFor(req => req)
+            .Must(req => req.AccountID != req.RecipientID)
+            .WithMessage("You cannot befriend yourself.")
             .Must(req => ObjectId.TryParse(req.RecipientID, out _))
             .WithMessage("Invalid RecipientID.")
             .MustAsync(async (req, _) => {

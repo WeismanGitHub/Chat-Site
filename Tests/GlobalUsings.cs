c4dd275bbc3da0157036042b1e279cf776290693
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-29 06:46:40 -0700

    merged GlobalUsings.cs and Metadata.cs

--- Tests/GlobalUsings.cs
+++ Tests/GlobalUsings.cs
@@ -1,1 +1,8 @@
 ﻿global using Microsoft.Extensions.DependencyInjection;
+global using FastEndpoints.Testing;
+global using Xunit.Abstractions;
+global using FluentAssertions;
+global using Xunit.Priority;
+global using FastEndpoints;
+global using System.Net;
+global using Xunit;

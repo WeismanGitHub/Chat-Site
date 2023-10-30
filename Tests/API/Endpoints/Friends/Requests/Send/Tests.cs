827dde9d3228d4ba1b77e7f93c2afa44257d15f5
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-29 23:22:24 -0700

    added some tests

--- Tests/API/Endpoints/Friends/Requests/Send/Tests.cs
+++ Tests/API/Endpoints/Friends/Requests/Send/Tests.cs
@@ -21,4 +21,52 @@ public class Tests : TestClass<Fixture> {
 
         res.IsSuccessStatusCode.Should().BeTrue();
     }
+
+    [Fact]
+    public async Task Invalid_RecipientID() {
+        await Fixture.SignClientIn(new Signin.Request() {
+            Email = ValidAccount.Email,
+            Password = ValidAccount.Password,
+        });
+
+        var res = await Fixture.Client.POSTAsync<Endpoint, Request>(new() {
+            AccountID = Fixture.UserID1,
+            Message = "Let's be friends.",
+            RecipientID = "invalid id"
+        });
+
+        res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task Message_Too_Long() {
+        await Fixture.SignClientIn(new Signin.Request() {
+            Email = ValidAccount.Email,
+            Password = ValidAccount.Password,
+        });
+
+        var res = await Fixture.Client.POSTAsync<Endpoint, Request>(new() {
+            AccountID = Fixture.UserID1,
+            Message = new string('*', 251),
+            RecipientID = "invalid id"
+        });
+
+        res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task Message_Too_Short() {
+        await Fixture.SignClientIn(new Signin.Request() {
+            Email = ValidAccount.Email,
+            Password = ValidAccount.Password,
+        });
+
+        var res = await Fixture.Client.POSTAsync<Endpoint, Request>(new() {
+            AccountID = Fixture.UserID1,
+            Message = "",
+            RecipientID = "invalid id"
+        });
+
+        res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
 }

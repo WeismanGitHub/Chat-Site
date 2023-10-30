21c35b9e8ca189a1c792557c3318398378fce98f
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-30 00:30:27 -0700

    improved validation

--- Source/API/Endpoints/Account/Update/Models.cs
+++ Source/API/Endpoints/Account/Update/Models.cs
@@ -1,4 +1,6 @@
-﻿namespace API.Endpoints.Account.Update;
+﻿using System.Net;
+
+namespace API.Endpoints.Account.Update;
 
 sealed class Request {
     [From(Claim.AccountID, IsRequired = true)]
@@ -10,17 +12,24 @@ sealed class Request {
 
 internal sealed class Validator : Validator<Request> {
     public Validator() {
+        RuleFor(req => req)
+            .Must(req => req.DisplayName != null || req.Email != null || req.Password != null)
+            .WithMessage("Must modify something");
+
         RuleFor(account => account.DisplayName)
             .MinimumLength(1).WithMessage("DisplayName cannot be empty.")
             .MaximumLength(User.MaxNameLength).WithMessage($"DisplayName cannot be longer than {User.MaxNameLength} characters.");
 
         RuleFor(account => account.Email)
-            .EmailAddress().WithMessage("The format of your email address is invalid.");
+            .EmailAddress().WithMessage("The format of your email address is invalid.")
+            .MustAsync(async (email, _) => await DB.Find<User>().Match(u => u.Email == email).ExecuteAnyAsync())
+            .WithErrorCode(HttpStatusCode.Conflict.ToString())
+            .WithMessage("Email is taken.");
 
         RuleFor(account => account.Password)
             .Must(password => {
                 if (password == null) return true;
                 return password.IsAValidPassword();
-        }).WithMessage("Password is invalid.");
+            }).WithMessage("Password is invalid.");
     }
 }

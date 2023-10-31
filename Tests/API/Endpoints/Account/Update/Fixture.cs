df58f6e7e190ad4008238dd2261b63363fe81e6f
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-31 05:37:54 -0700

    created fixture

---
+++ Tests/API/Endpoints/Account/Update/Fixture.cs
@@ -0,0 +1,30 @@
+﻿using APISignin = API.Endpoints.Account.Signin;
+using API.Database.Entities;
+using MongoDB.Entities;
+using MongoDB.Bson;
+
+namespace Tests.Account.Update;
+
+public class Fixture : TestFixture<Program> {
+    public Fixture(IMessageSink sink) : base(sink) { }
+
+	public string AccountID = ObjectId.GenerateNewId().ToString();
+
+    protected override async Task SetupAsync() {
+		await DB.InsertAsync(new User() {
+			ID = AccountID,
+			DisplayName = ValidAccount.DisplayName,
+			Email = ValidAccount.Email,
+			PasswordHash = BCrypt.Net.BCrypt.HashPassword(ValidAccount.Password)
+		});
+
+		await Client.POSTAsync<APISignin.Endpoint, APISignin.Request>(new APISignin.Request() {
+			Email = ValidAccount.Email,
+			Password = ValidAccount.Password,
+		});
+	}
+
+	protected override async Task TearDownAsync() {
+        await DB.DeleteAsync<User>(u => u.Email == ValidAccount.Email);
+    }
+}

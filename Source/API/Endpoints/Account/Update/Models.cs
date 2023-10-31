7c6e539b0adb51e7f39191812e56f6be2fe2d8f0
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-10-31 05:44:29 -0700

    fixed validation

--- Source/API/Endpoints/Account/Update/Models.cs
+++ Source/API/Endpoints/Account/Update/Models.cs
@@ -2,34 +2,56 @@
 
 namespace API.Endpoints.Account.Update;
 
+public class NewData {
+	public string? DisplayName { get; set; }
+	public string? Email { get; set; }
+	public string? Password { get; set; }
+}
+
 public sealed class Request {
     [From(Claim.AccountID, IsRequired = true)]
-    public string? AccountID { get; set; }
-    public string? DisplayName { get; set; }
-    public string? Email { get; set; }
-    public string? Password { get; set; }
+    public string AccountID { get; set; }
+	public required NewData NewData { get; set; }
+	public required string CurrentPassword { get; set; }
 }
 
 internal sealed class Validator : Validator<Request> {
     public Validator() {
-        RuleFor(req => req)
+		RuleFor(req => req)
+			.MustAsync(async (req, _) => {
+				User? account = await DB.Find<User>().MatchID(req.AccountID).ExecuteSingleAsync();
+				return account == null ? false : BCrypt.Net.BCrypt.Verify(req.CurrentPassword, account.PasswordHash);
+			})
+			.WithErrorCode(nameof(HttpStatusCode.Unauthorized))
+			.WithMessage("Invalid password.");
+
+		RuleFor(req => req.NewData)
             .Must(req => req.DisplayName != null || req.Email != null || req.Password != null)
             .WithMessage("Must modify something");
 
-        RuleFor(account => account.DisplayName)
-            .MinimumLength(1).WithMessage("DisplayName cannot be empty.")
-            .MaximumLength(User.MaxNameLength).WithMessage($"DisplayName cannot be longer than {User.MaxNameLength} characters.");
+		When(req => req.NewData.DisplayName != null, () => {
+			RuleFor(account => account.NewData.DisplayName)
+			.NotEmpty()
+			.MinimumLength(1).WithMessage("DisplayName cannot be empty.")
+			.MaximumLength(User.MaxNameLength).WithMessage($"DisplayName cannot be longer than {User.MaxNameLength} characters.");
+		});
 
-        RuleFor(account => account.Email)
-            .EmailAddress().WithMessage("The format of your email address is invalid.")
-            .MustAsync(async (email, _) => await DB.Find<User>().Match(u => u.Email == email).ExecuteAnyAsync())
-            .WithErrorCode(HttpStatusCode.Conflict.ToString())
-            .WithMessage("Email is taken.");
+		When(req => req.NewData.Email != null, () => {
+			RuleFor(account => account.NewData.Email)
+				.NotEmpty()
+				.EmailAddress().WithMessage("The format of your email address is invalid.")
+				.MustAsync(async (email, _) => await DB.Find<User>().Match(u => u.Email == email).ExecuteAnyAsync())
+				.WithErrorCode(HttpStatusCode.Conflict.ToString())
+				.WithMessage("Email is taken.");
+		});
 
-        RuleFor(account => account.Password)
-            .Must(password => {
-                if (password == null) return true;
-                return password.IsAValidPassword();
-            }).WithMessage("Password is invalid.");
-    }
+		When(req => req.NewData.Password != null, () => {
+			RuleFor(account => account.NewData.Password)
+				.NotEmpty()
+				.Must(password => {
+					if (password == null) return true;
+					return password.IsAValidPassword();
+				}).WithMessage("Password is invalid.");
+		});
+	}
 }

35e1833ee7b7622484e49d6327d0697955d91285
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-11-02 11:16:25 -0700

    implemented email tests

--- Tests/API/Endpoints/Account/Update/Tests.cs
+++ Tests/API/Endpoints/Account/Update/Tests.cs
@@ -1,9 +1,11 @@
 using API.Endpoints.Account.Update;
 using API.Database.Entities;
 using MongoDB.Entities;
+using Microsoft.Identity.Client;
 
 namespace Tests.Account.Update;
 
+[DefaultPriority(0)]
 public class Tests : TestClass<Fixture> {
     public Tests(Fixture fixture, ITestOutputHelper output) : base(fixture, output) { }
 
@@ -29,141 +31,146 @@ public class Tests : TestClass<Fixture> {
 		BCrypt.Net.BCrypt.Verify(ValidAccount.Password + "new", acc!.PasswordHash).Should().BeTrue();
     }
 
-    //[Fact, Priority(2)]
-    //public async Task Taken_Email() {
-    //    var res = await Fixture.Client.POSTAsync<Endpoint, Request>(new() {
-    //        DisplayName = ValidAccount.Email,
-    //        Email = ValidAccount.Email,
-    //        Password = ValidAccount.Password
-    //    });
+	[Fact]
+	public async Task Taken_Email() {
+		await DB.InsertAsync(new User() {
+			DisplayName = ValidAccount.DisplayName,
+			Email = "taken" + ValidAccount.Email,
+			PasswordHash = BCrypt.Net.BCrypt.HashPassword(ValidAccount.Password)
+		});
 
-    //    res.StatusCode.Should().Be(HttpStatusCode.Conflict);
-    //}
+		var res = await Fixture.Client.PATCHAsync<Endpoint, Request>(new() {
+			AccountID = Fixture.AccountID,
+			NewData = new() {
+				DisplayName = "New DisplayName",
+				Email = "taken" + ValidAccount.Email,
+				Password = ValidAccount.Password + "new"
+			},
+			CurrentPassword = ValidAccount.Password
+		});
 
-    //[Fact]
-    //public async Task Empty_DisplayName() {
-    //    var res = await Fixture.Client.POSTAsync<Endpoint, Request>(new() {
-    //        DisplayName = "",
-    //        Email = ValidAccount.Email,
-    //        Password = ValidAccount.Password
-    //    });
+		await DB.DeleteAsync<User>((u) => u.Email == "taken" + ValidAccount.Email);
+		res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+	}
 
-    //    res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-    //}
+	[Fact]
+	public async Task Empty_Email() {
+		var res = await Fixture.Client.PATCHAsync<Endpoint, Request>(new() {
+			AccountID = Fixture.AccountID,
+			NewData = new() {
+				DisplayName = "New DisplayName",
+				Email = "",
+				Password = ValidAccount.Password + "new"
+			},
+			CurrentPassword = ValidAccount.Password
+		});
 
-    //[Fact]
-    //public async Task DisplayName_Too_Long() {
-    //    var res = await Fixture.Client.POSTAsync<Endpoint, Request>(new() {
-    //        Email = ValidAccount.Email,
-    //        Password = ValidAccount.Password
-    //    });
+		res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+	}
 
-    //    res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-    //}
-    
-    //[Fact]
-    //public async Task Null_DisplayName() {
-    //    var res = await Fixture.Client.POSTAsync<Endpoint, Request>(new() {
-    //        Password = ValidAccount.Password
-    //    });
+	//[Fact]
+	//public async Task Empty_DisplayName() {
+	//    var res = await Fixture.Client.POSTAsync<Endpoint, Request>(new() {
+	//        DisplayName = "",
+	//        Email = ValidAccount.Email,
+	//        Password = ValidAccount.Password
+	//    });
 
-    //    res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-    //}
-    
-    //[Fact]
-    //public async Task Empty_Email() {
-    //    var res = await Fixture.Client.POSTAsync<Endpoint, Request>(new() {
-    //        DisplayName = ValidAccount.DisplayName,
-    //        Email = "",
-    //        Password = ValidAccount.Password
-    //    });
+	//    res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+	//}
 
-    //    res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-    //}
+	//[Fact]
+	//public async Task DisplayName_Too_Long() {
+	//    var res = await Fixture.Client.POSTAsync<Endpoint, Request>(new() {
+	//        Email = ValidAccount.Email,
+	//        Password = ValidAccount.Password
+	//    });
 
-    //[Fact]
-    //public async Task Null_Email() {
-    //    var res = await Fixture.Client.POSTAsync<Endpoint, Request>(new() {
-    //        DisplayName = ValidAccount.DisplayName,
-    //        Password = ValidAccount.Password
-    //    });
+	//    res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+	//}
 
-    //    res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-    //}
+	//[Fact]
+	//public async Task Null_DisplayName() {
+	//    var res = await Fixture.Client.POSTAsync<Endpoint, Request>(new() {
+	//        Password = ValidAccount.Password
+	//    });
 
-    //[Fact]
-    //public async Task EmptyPassword() {
-    //    var res = await Fixture.Client.POSTAsync<Endpoint, Request>(new() {
-    //        DisplayName = "",
-    //        Email = ValidAccount.Email,
-    //        Password = ValidAccount.Password
-    //    });
+	//    res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+	//}
 
-    //    res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-    //}
+	//[Fact]
+	//public async Task EmptyPassword() {
+	//    var res = await Fixture.Client.POSTAsync<Endpoint, Request>(new() {
+	//        DisplayName = "",
+	//        Email = ValidAccount.Email,
+	//        Password = ValidAccount.Password
+	//    });
 
-    //[Fact]
-    //public async Task Null_Password() {
-    //    var res = await Fixture.Client.POSTAsync<Endpoint, Request>(new() {
-    //        Email = ValidAccount.Email,
-    //        Password = ValidAccount.Password
-    //    });
+	//    res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+	//}
 
-    //    res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-    //}
+	//[Fact]
+	//public async Task Null_Password() {
+	//    var res = await Fixture.Client.POSTAsync<Endpoint, Request>(new() {
+	//        Email = ValidAccount.Email,
+	//        Password = ValidAccount.Password
+	//    });
 
-    //[Fact]
-    //public async Task Password_Too_Long() {
-    //    var res = await Fixture.Client.POSTAsync<Endpoint, Request>(new() {
-    //        DisplayName = ValidAccount.DisplayName,
-    //        Email = ValidAccount.Email,
-    //        Password = "VP1" + new string('*', 68) // VP1 is to meet the other requirements.
-    //    });
+	//    res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+	//}
 
-    //    res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-    //}
+	//[Fact]
+	//public async Task Password_Too_Long() {
+	//    var res = await Fixture.Client.POSTAsync<Endpoint, Request>(new() {
+	//        DisplayName = ValidAccount.DisplayName,
+	//        Email = ValidAccount.Email,
+	//        Password = "VP1" + new string('*', 68) // VP1 is to meet the other requirements.
+	//    });
 
-    //[Fact]
-    //public async Task Password_Too_Short() {
-    //    var res = await Fixture.Client.POSTAsync<Endpoint, Request>(new() {
-    //        DisplayName = ValidAccount.DisplayName,
-    //        Email = ValidAccount.Email,
-    //        Password = "VP1" + new string('*', 6) // VP1 is to meet the other requirements.
-    //    });
+	//    res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+	//}
 
-    //    res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-    //}
-    
-    //[Fact]
-    //public async Task Password_Missing_Digit() {
-    //    var res = await Fixture.Client.POSTAsync<Endpoint, Request>(new() {
-    //        DisplayName = ValidAccount.DisplayName,
-    //        Email = ValidAccount.Email,
-    //        Password = "InvalidPassword"
-    //    });
+	//[Fact]
+	//public async Task Password_Too_Short() {
+	//    var res = await Fixture.Client.POSTAsync<Endpoint, Request>(new() {
+	//        DisplayName = ValidAccount.DisplayName,
+	//        Email = ValidAccount.Email,
+	//        Password = "VP1" + new string('*', 6) // VP1 is to meet the other requirements.
+	//    });
 
-    //    res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-    //}    
+	//    res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+	//}
 
-    //[Fact]
-    //public async Task Password_Missing_Uppercase() {
-    //    var res = await Fixture.Client.POSTAsync<Endpoint, Request>(new() {
-    //        DisplayName = ValidAccount.DisplayName,
-    //        Email = ValidAccount.Email,
-    //        Password = "invalidpassword1"
-    //    });
+	//[Fact]
+	//public async Task Password_Missing_Digit() {
+	//    var res = await Fixture.Client.POSTAsync<Endpoint, Request>(new() {
+	//        DisplayName = ValidAccount.DisplayName,
+	//        Email = ValidAccount.Email,
+	//        Password = "InvalidPassword"
+	//    });
 
-    //    res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-    //}
+	//    res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+	//}    
 
-    //[Fact]
-    //public async Task Password_Missing_Lowercase() {
-    //    var res = await Fixture.Client.POSTAsync<Endpoint, Request>(new() {
-    //        DisplayName = ValidAccount.DisplayName,
-    //        Email = ValidAccount.Email,
-    //        Password = "INVALIDPASSWORD1"
-    //    });
+	//[Fact]
+	//public async Task Password_Missing_Uppercase() {
+	//    var res = await Fixture.Client.POSTAsync<Endpoint, Request>(new() {
+	//        DisplayName = ValidAccount.DisplayName,
+	//        Email = ValidAccount.Email,
+	//        Password = "invalidpassword1"
+	//    });
 
-    //    res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-    //}
+	//    res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+	//}
+
+	//[Fact]
+	//public async Task Password_Missing_Lowercase() {
+	//    var res = await Fixture.Client.POSTAsync<Endpoint, Request>(new() {
+	//        DisplayName = ValidAccount.DisplayName,
+	//        Email = ValidAccount.Email,
+	//        Password = "INVALIDPASSWORD1"
+	//    });
+
+	//    res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+	//}
 }

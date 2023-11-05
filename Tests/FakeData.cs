90fa34e2f9a4171856a27d3cd8b3c4e7eaa339eb
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-11-04 17:19:02 -0700

    created FakeData class

---
+++ Tests/FakeData.cs
@@ -0,0 +1,23 @@
+﻿using API.Database.Entities;
+using Bogus;
+
+using MongoDB.Bson;
+
+internal static class FakeData {
+	private static Faker<User> UserFaker { get; set; }
+	static FakeData() {
+		Randomizer.Seed = new Random(12345);
+		Faker.DefaultStrictMode = true;
+
+		UserFaker = new Faker<User>()
+			.RuleFor(u => u.ID, f => ObjectId.GenerateNewId().ToString())
+			.RuleFor(u => u.DisplayName, f => f.Name.FirstName())
+			.RuleFor(u => u.Email, f => f.Internet.Email())
+			.RuleFor(u => u.PasswordHash, f => BCrypt.Net.BCrypt.HashPassword(f.Internet.Password(length: 15, prefix: "Pw1")));
+
+	}
+
+	public static List<User> GenerateUsers(int amount) {
+		return UserFaker.GenerateForever().Take(amount).ToList();
+	}
+}

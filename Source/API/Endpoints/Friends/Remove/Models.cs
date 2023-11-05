a54f2d5ca2a77d0569dab796f8880237d4eedce5
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-11-05 14:09:17 -0800

    created validator/request

---
+++ Source/API/Endpoints/Friends/Remove/Models.cs
@@ -0,0 +1,16 @@
+﻿namespace API.Endpoints.Friends.Remove;
+
+public sealed class Request {
+    [From(Claim.AccountID, IsRequired = true)]
+    public string AccountID { get; set; }
+	public string FriendID { get; set; }
+}
+
+internal sealed class Validator : Validator<Request> {
+	public Validator() {
+		RuleFor(req => req.FriendID)
+			.NotEmpty()
+			.Must(id => ObjectId.TryParse(id, out _))
+			.WithMessage("Invalid FriendID.");
+	}
+}

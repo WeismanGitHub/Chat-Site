d48acbbbc531af210a0f446763715362419d87b1
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-11-05 13:58:52 -0800

    Revert "deleted"
    This reverts commit 7ee6f740719364275f7620759dd1e6c3a9d46269.

---
+++ Tests/API/Endpoints/Friends/Get/Tests.cs
@@ -0,0 +1,33 @@
+using API.Endpoints.Friends.Get;
+using API.Database.Entities;
+using MongoDB.Entities;
+using MongoDB.Bson;
+
+namespace Tests.Friends.Requests.Get;
+
+public class Tests : TestClass<Fixture> {
+    public Tests(Fixture fixture, ITestOutputHelper output) : base(fixture, output) { }
+
+    [Fact]
+    public async Task Valid() {
+		var (res, rsp) = await Fixture.Client.GETAsync<Endpoint, Request, List<User>>(
+			new() { AccountID = Fixture.AccountID }
+		);
+
+        //res.IsSuccessStatusCode.Should().BeTrue();
+		res.StatusCode.Should().Be(HttpStatusCode.OK);
+
+		var account = await DB.Find<User>().MatchID(Fixture.AccountID).ExecuteSingleAsync();
+		var friends = await DB
+			.Find<User>()
+				.Match(u => account!.FriendIDs.Contains(u.ID))
+				.Project(u => new() {
+					DisplayName = u.DisplayName,
+					ID = u.ID,
+					CreatedAt = u.CreatedAt
+				})
+				.ExecuteAsync();
+
+		rsp.All(f => friends!.Contains(f)).Should().BeTrue();
+    }
+}

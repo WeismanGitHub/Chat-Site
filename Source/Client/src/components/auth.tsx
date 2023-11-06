de95ced75631d14e1d5f09ddb85962fd21a6a4c8
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-11-05 16:01:36 -0800

    created page

---
+++ Source/Client/src/components/auth.tsx
@@ -0,0 +1,22 @@
+// import { useQuery } from '@tanstack/react-query'
+import { Form } from 'react-bootstrap';
+// import ky from 'ky';
+
+
+export default function Auth() {
+    // const { isLoading, error, data } = useQuery({
+    //     queryKey: ['data'],
+    //     queryFn: () => ky.get('https://localhost:7005/').text(),
+    // })
+
+    // if (error) return 'An error has occurred: ' + (error instanceof Error ? error.message : 'unknown')
+  
+    return (<>
+        <Form>
+        <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+        </div>
+        </Form>
+    </>)
+}

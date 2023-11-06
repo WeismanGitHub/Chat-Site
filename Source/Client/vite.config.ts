5c03bb72aa1a05bee7c881eb33cafc097c761a9b
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-11-06 10:52:14 -0800

    changed build location

--- Source/Client/vite.config.ts
+++ Source/Client/vite.config.ts
@@ -23,6 +23,9 @@ const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        outDir: '../../dist'
+    },
     plugins: [plugin()],
     resolve: {
         alias: {

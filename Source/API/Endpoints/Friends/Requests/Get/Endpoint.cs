812b41bfb3e89760446cc88996c0daa330b4606f
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-11-07 14:33:20 -0800

    created endpoint

---
+++ Source/API/Endpoints/Friends/Requests/Get/Endpoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace API.Endpoints.Friends.Requests.Get;
+
+public sealed class Endpoint : Endpoint<Request> {
+    public override void Configure() {
+        Post("/");
+        Group<RequestGroup>();
+        Version(1);
+        
+        Summary(settings => {
+            settings.Summary = "Send a friend request to a user.";
+        });
+    }
+
+    public override async Task<List<FriendRequest>> HandleAsync(Request req, CancellationToken cancellationToken) {
+		var query = DB.PagedSearch<FriendRequest>().PageSize(10).PageNumber(1);
+
+        if (req.FriendReqType == FriendRequestType.Incoming) {
+			query.Match(fr => fr.RecipientID == req.AccountID);
+        } else {
+			query.Match(fr => fr.RequesterID == req.AccountID);
+        }
+
+        var res = await query.ExecuteAsync();
+		return res.Results.ToList();
+    }
+}
---
+++ Source/API/Endpoints/Friends/Requests/Get/Models.cs
@@ -0,0 +1,15 @@
+﻿namespace API.Endpoints.Friends.Requests.Get;
+
+public enum FriendRequestType {
+	Incoming,
+	Outgoing
+}
+
+public sealed class Request {
+    [From(Claim.AccountID, IsRequired = true)]
+    public string AccountID { get; set; }
+	[QueryParam]
+	public FriendRequestType? FriendReqType { get; set; } = FriendRequestType.Incoming;
+	[QueryParam]
+	public int? Page { get; set; } = 0;
+}

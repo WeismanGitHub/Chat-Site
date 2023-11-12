2810ae752c46c7e0d0bc472dd06b26528b9ff8f4
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-11-11 19:11:03 -0800

    changed how conversations are stored

--- Source/API/Database/Entities/Conversation.cs
+++ Source/API/Database/Entities/Conversation.cs
@@ -1,16 +1,16 @@
-﻿namespace API.Database.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Database.Entities;
 
 [Collection("Conversations")]
 public class Conversation : Entity {
     [BsonId, BsonRepresentation(BsonType.ObjectId)]
     public string Name { get; set; }
-    public Many<User> Members { get; set; } = new();
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+	[MaxLength(100, ErrorMessage = "Cannot have more than 100 members.")]
+	public List<string> MemberIDs { get; set; } = new List<string>();
+	public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public string CollectionAttribute() {
         return "Conversations";
-    }
-
-    public Conversation() {
-        this.InitManyToMany(() => Members, member => member.ID);
     }
 }
--- Source/API/Database/Entities/User.cs
+++ Source/API/Database/Entities/User.cs
@@ -14,11 +14,7 @@ public class User : Entity {
     public string PasswordHash { get; set; }
     [MaxLength(100, ErrorMessage = "Cannot add more than 100 friends.")]
     public List<string> FriendIDs { get; set; } = new List<string>();
-    [MaxLength(100, ErrorMessage = "Cannot join more than 100 conversations.")]
-    public Many<Conversation> Conversations { get; set; }
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-
-    public User() {
-        //this.InitManyToMany(() => Conversations, convo => convo.Members);
-    }
+	[MaxLength(100, ErrorMessage = "Cannot join more than 100 conversations.")]
+	public List<string> ConversationIDs { get; set; } = new List<string>();
+	public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }

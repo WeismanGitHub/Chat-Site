6b4e381967ae9f3b81f7af7051eb7dc5ec6d4326
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-11-11 19:29:26 -0800

    added min/max attributes for convo name

--- Source/API/Database/Entities/Conversation.cs
+++ Source/API/Database/Entities/Conversation.cs
@@ -4,13 +4,13 @@ namespace API.Database.Entities;
 
 [Collection("Conversations")]
 public class Conversation : Entity {
-    [BsonId, BsonRepresentation(BsonType.ObjectId)]
+	public const int MaxNameLength = 25;
+	public const int MinNameLength = 1;
+
+	[BsonId, BsonRepresentation(BsonType.ObjectId)]
     public string Name { get; set; }
 
 	[MaxLength(100, ErrorMessage = "Cannot have more than 100 members.")]
 	public List<string> MemberIDs { get; set; } = new List<string>();
 	public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public string CollectionAttribute() {
-        return "Conversations";
-    }
 }

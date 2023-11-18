0de4ee7800cf7d43801f0af4f4796dba1c4fbf73
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-11-17 21:12:02 -0800

    IT WORKS

--- Source/Client/src/components/signin.tsx
+++ Source/Client/src/components/signin.tsx
@@ -1,98 +1,66 @@
-import { Form, Button, Col, InputGroup, Row } from 'react-bootstrap';
-import { useState } from 'react';
+import { Button, Col, Form, InputGroup, Row } from 'react-bootstrap';
+import * as formik from 'formik';
+import * as yup from 'yup';
 
 export default function Signin() {
-    const [validated, setValidated] = useState(false);
+    const { Formik } = formik;
 
-    const handleSubmit = (event: unknown) => {
-        console.log(event);
-        // const form = event.currentTarget;
+    const schema = yup.object().shape({
+        email: yup.string().required().email("Must be a valid email."),
+        password: yup.string().required().min(10).max(70),
+    });
 
-        // if (form.checkValidity() === false) {
-        //     event.preventDefault();
-        // event.stopPropagation();
-        // }
-
-        setValidated(true);
-    };
-
-    return (
-        <Form noValidate validated={validated} onSubmit={handleSubmit}>
-            <Row className="mb-3">
-                <Form.Group as={Col} md="4" controlId="validationCustom01">
-                    <Form.Label>First name</Form.Label>
-                    <Form.Control
-                        required
-                        type="text"
-                        placeholder="First name"
-                        defaultValue="Mark"
-                    />
-                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group as={Col} md="4" controlId="validationCustom02">
-                    <Form.Label>Last name</Form.Label>
-                    <Form.Control
-                        required
-                        type="text"
-                        placeholder="Last name"
-                        defaultValue="Otto"
-                    />
-                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group
-                    as={Col}
-                    md="4"
-                    controlId="validationCustomUsername"
-                >
-                    <Form.Label>Username</Form.Label>
-                    <InputGroup hasValidation>
-                        <InputGroup.Text id="inputGroupPrepend">
-                            @
-                        </InputGroup.Text>
-                        <Form.Control
-                            type="text"
-                            placeholder="Username"
-                            aria-describedby="inputGroupPrepend"
-                            required
-                        />
+  return (<Formik
+    validationSchema={schema}
+    validateOnChange
+        onSubmit={(values) => {
+            console.log(values)
+        }}
+        initialValues={{
+            email: '',
+            password: '',
+        }}
+    >
+        {({ handleSubmit, handleChange, values, errors }) => (
+            <Form noValidate onSubmit={handleSubmit}>
+                <Row className="mb-3">
+                    <Form.Group as={Col} controlId="EmailID">
+                        <Form.Label>Email</Form.Label>
+                        <InputGroup hasValidation>
+                            <Form.Control
+                                type="email"
+                                placeholder="example@email.com"
+                                name="email"
+                                value={values.email}
+                                onChange={handleChange}
+                                isInvalid={!!errors.email}
+                            />
+                        </InputGroup>
                         <Form.Control.Feedback type="invalid">
-                            Please choose a username.
+                            {errors.email}
                         </Form.Control.Feedback>
-                    </InputGroup>
-                </Form.Group>
-            </Row>
-            <Row className="mb-3">
-                <Form.Group as={Col} md="6" controlId="validationCustom03">
-                    <Form.Label>City</Form.Label>
-                    <Form.Control type="text" placeholder="City" required />
-                    <Form.Control.Feedback type="invalid">
-                        Please provide a valid city.
-                    </Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group as={Col} md="3" controlId="validationCustom04">
-                    <Form.Label>State</Form.Label>
-                    <Form.Control type="text" placeholder="State" required />
-                    <Form.Control.Feedback type="invalid">
-                        Please provide a valid state.
-                    </Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group as={Col} md="3" controlId="validationCustom05">
-                    <Form.Label>Zip</Form.Label>
-                    <Form.Control type="text" placeholder="Zip" required />
-                    <Form.Control.Feedback type="invalid">
-                        Please provide a valid zip.
-                    </Form.Control.Feedback>
-                </Form.Group>
-            </Row>
-            <Form.Group className="mb-3">
-                <Form.Check
-                    required
-                    label="Agree to terms and conditions"
-                    feedback="You must agree before submitting."
-                    feedbackType="invalid"
-                />
-            </Form.Group>
-            <Button type="submit">Submit form</Button>
-        </Form>
-    );
+                    </Form.Group>
+                </Row>
+                <Row className="mb-3">
+                    <Form.Group as={Col} controlId="PasswordID">
+                        <Form.Label>Password</Form.Label>
+                        <InputGroup hasValidation>
+                            <Form.Control
+                                type="password"
+                                aria-describedby="inputGroupPrepend"
+                                name="password"
+                                value={values.password}
+                                onChange={handleChange}
+                                isInvalid={!!errors.password}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                {errors.password}
+                            </Form.Control.Feedback>
+                        </InputGroup>
+                    </Form.Group>
+                </Row>
+                <Button type="submit">Sign In</Button>
+            </Form>
+        )}
+    </Formik>);
 }

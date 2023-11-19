0c37c7d1fd62096b4d227eb6711a1e8c5b915796
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-11-19 02:28:33 -0800

    moved Friends part

--- Source/Client/src/pages/home.tsx
+++ Source/Client/src/pages/home.tsx
@@ -1,34 +1,9 @@
 import Navbar from '../components/navbar';
-// import { useQuery } from '@tanstack/react-query';
-// import ky from 'ky';
-
-// type friend = {
-//     ID: string;
-//     DisplayName: string;
-//     CreatedAt: string;
-// };
+import Friends from '../components/friends';
 
 export default function Home() {
-    return <Navbar />;
-    // const { isLoading, error, data } = useQuery({
-    //     queryKey: ['data'],
-    //     queryFn: (): Promise<friend[]> => ky.get('/api/friends').json(),
-    // });
-
-    // if (error) {
-    //     throw error;
-    // }
-
-    // return (
-    //     <div>
-    //         {(isLoading == true ? [] : data!).map((friend) => {
-    //             return (
-    //                 <>
-    //                     {friend.DisplayName}
-    //                     {friend.CreatedAt}
-    //                 </>
-    //             );
-    //         })}
-    //     </div>
-    // );
+    return <>
+        <Navbar />
+        <Friends />
+    </>
 }

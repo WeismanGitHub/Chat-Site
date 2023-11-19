700380f5a0e66afd62fae0b5e8ce977675698fa4
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-11-18 20:29:15 -0800

    made languages/frameworks/libraries bold

--- Source/Client/src/pages/about.tsx
+++ Source/Client/src/pages/about.tsx
@@ -18,10 +18,9 @@ export default function About() {
             <div className="bg-white rounded shadow card-body p-3 text-center fs-5">
                 <p>
                     This website provides users with a platform for connecting, adding friends, and engaging in real-time one-on-one or group chats, among other features.
-
                 </p>
                 <p>
-                    I'm revisiting a <a href="https://github.com/WeismanGitHub/Chat-Website" className="link-underline-primary">prior project</a> I initially developed with just JavaScript. However, now, I've strategically chosen to harness the capabilities of ASP.NET and the FastEndpoints library, ensuring a robust and scalable infrastructure. Additionally, with the powerful combination of Typescript, React, and Bootstrap, I've created a well-designed and easy to use front-end.
+                    I'm revisiting a <a href="https://github.com/WeismanGitHub/Chat-Website" className="link-underline-primary">prior project</a> I initially developed with <strong>JavaScript</strong>. However, now, I've strategically chosen to harness the capabilities of <strong>ASP.NET</strong> and the <strong>FastEndpoints</strong> library, ensuring a robust and scalable infrastructure. Additionally, with the powerful combination of <strong>Typescript, React, and Bootstrap</strong>, I've created a well-designed and easy to use front-end.
                 </p>
                 <p>
                     The driving force behind this endeavor is my unwavering commitment to elevating my C# programming skills to new levels of mastery. By revisiting and enhancing my prior work, I aim to not only reinforce my existing knowledge but also push the boundaries of what I can achieve.

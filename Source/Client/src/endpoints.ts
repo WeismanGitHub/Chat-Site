839ce3b816dd0824fb3e4ec91a615f6d3e7d7c99
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-11-18 23:13:54 -0800

    added a route

--- Source/Client/src/endpoints.ts
+++ Source/Client/src/endpoints.ts
@@ -1,5 +1,6 @@
 export default class Endpoints {
-    public static readonly Signin = "/API/Account/Signin/v1"
-    public static readonly Signup = "/API/Account/Signup/v1"
-    public static readonly Account = "/API/Account/v1"
+    public static readonly Signin = '/API/Account/Signin/v1';
+    public static readonly Signup = '/API/Account/Signup/v1';
+    public static readonly Signout = '/API/Account/Signout/v1';
+    public static readonly Account = '/API/Account/v1';
 }

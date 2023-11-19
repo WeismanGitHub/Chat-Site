21549e496791def8d6086909156f41a06c2273d2
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-11-19 01:32:17 -0800

    added endpoint

--- Source/Client/src/endpoints.ts
+++ Source/Client/src/endpoints.ts
@@ -1,6 +1,8 @@
 export default class Endpoints {
+    public static readonly Account = '/API/Account/v1';
     public static readonly Signin = '/API/Account/Signin/v1';
     public static readonly Signup = '/API/Account/Signup/v1';
     public static readonly Signout = '/API/Account/Signout/v1';
-    public static readonly Account = '/API/Account/v1';
+
+    public static readonly Friends = '/API/Friends/v1';
 }

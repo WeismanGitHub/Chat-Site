dffd29e79882e2fd96fdacc200e0be79e60b9864
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-11-19 06:23:24 -0800

    redirect to /auth if not logged in

--- Source/Client/src/pages/home.tsx
+++ Source/Client/src/pages/home.tsx
@@ -1,9 +1,24 @@
-import Navbar from '../components/navbar';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Friends from '../components/friends';
+import Navbar from '../components/navbar';
 
 export default function Home() {
-    return <>
-        <Navbar />
-        <Friends />
-    </>
+    const loggedIn = localStorage.getItem('loggedIn');
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!loggedIn) {
+            navigate('/auth');
+        }
+    }, [])
+    
+    return (
+        <div>
+            <Navbar />
+            <div className='w-25 row vh-100 m-1 text-center'>
+                <Friends />
+            </div>
+        </div>
+    );
 }

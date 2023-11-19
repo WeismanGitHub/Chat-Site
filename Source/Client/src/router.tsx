cdc685816ce4911dce06d1dbabb11078de102c58
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-11-18 18:03:05 -0800

    added /about route

--- Source/Client/src/router.tsx
+++ Source/Client/src/router.tsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
 
+import About from './pages/about.tsx';
 import Auth from './pages/auth.tsx';
 import Home from './pages/home.tsx';
 
 const router = createBrowserRouter([
+    { path: '/About', element: <About /> },
     { path: '/Auth', element: <Auth /> },
     { path: '/', element: <Home /> },
 ]);

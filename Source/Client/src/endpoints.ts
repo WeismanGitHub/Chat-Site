3c96ba1d64bb0f2e745a2fd64c976e7120c75400
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-11-18 17:38:49 -0800

    fixed file name

---
+++ Source/Client/src/endpoints.ts
@@ -0,0 +1,5 @@
+export default class Endpoints {
+    public static readonly Signin = "/API/Account/Signin/v1"
+    public static readonly Signup = "/API/Account/Signup/v1"
+    public static readonly Account = "/API/Account/v1"
+}

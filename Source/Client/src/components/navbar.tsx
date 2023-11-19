6a3000aa628c1f55c4652bcfe8d1a5007b828ca1
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-11-18 21:28:02 -0800

    functioning

--- Source/Client/src/components/navbar.tsx
+++ Source/Client/src/components/navbar.tsx
@@ -1,5 +1,14 @@
 export default function Navbar() {
-    return <div>
-        
-    </div>
+    return <nav className="navbar navbar-expand navbar-dark bg-primary ps-2 pe-2 justify-content-center">
+        <a className="navbar-brand" href="/">
+            <img src="/icon.png" width={50} height={50} alt="icon" className="me-2"/>
+            Chat Site v2
+        </a>
+
+        <div className="justify-content-start navbar-nav">
+            <a className="nav-item nav-link active" href="/">Home</a>
+            <a className="nav-item nav-link active" href="/about">About</a>
+            <a className="nav-item nav-link active" href="/auth">Signin/Signup</a>
+        </div>
+    </nav>
 }

399a56ade2d69e61cc63569c63ad52f4898ccc57
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-11-21 04:09:49 -0800

    added Account route

--- Source/Client/src/router.tsx
+++ Source/Client/src/router.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom';
 
+import Account from './pages/account.tsx';
 import About from './pages/about.tsx';
 import Auth from './pages/auth.tsx';
 import Home from './pages/home.tsx';
 
 const router = createBrowserRouter([
+    { path: '/Account', element: <Account /> },
     { path: '/About', element: <About /> },
     { path: '/Auth', element: <Auth /> },
     { path: '/', element: <Home /> },

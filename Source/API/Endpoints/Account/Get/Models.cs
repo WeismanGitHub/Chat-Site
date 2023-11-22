c2b6b8c4604db6f5bf9da9e1ce29b226e033d330
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-11-21 19:21:42 -0800

    created GET account endpoint

---
+++ Source/API/Endpoints/Account/Get/Models.cs
@@ -0,0 +1,15 @@
+﻿namespace API.Endpoints.Account.Get;
+
+sealed class Request {
+    [From(Claim.AccountID, IsRequired = true)]
+    public string AccountID { get; set; }
+}
+
+sealed class Response {
+	public string ID { get; set; }
+	public string DisplayName { get; set; }
+	public string Email { get; set; }
+	public int TotalFriends { get; set; }
+	public int TotalConversations { get; set; }
+	public DateTime CreatedAt { get; set; }
+}

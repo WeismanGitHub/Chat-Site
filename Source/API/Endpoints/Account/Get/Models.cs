ab24b6f6a9ff3d421b4ba27439433a1c2372fedc
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-11-23 00:08:17 -0800

    made classes public for testing

--- Source/API/Endpoints/Account/Get/Models.cs
+++ Source/API/Endpoints/Account/Get/Models.cs
@@ -1,11 +1,11 @@
 ﻿namespace API.Endpoints.Account.Get;
 
-sealed class Request {
+public sealed class Request {
     [From(Claim.AccountID, IsRequired = true)]
     public string AccountID { get; set; }
 }
 
-sealed class Response {
+public sealed class Response {
 	public string ID { get; set; }
 	public string DisplayName { get; set; }
 	public string Email { get; set; }

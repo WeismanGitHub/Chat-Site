8ec5ccbd7eb4c45633c4cb499ec8386072961e82
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-11-23 00:08:22 -0800

    created fixture

---
+++ Tests/API/Account/Get/Fixture.cs
@@ -0,0 +1,31 @@
+﻿using SigninAPI = API.Endpoints.Account.Signin;
+using API.Database.Entities;
+using MongoDB.Entities;
+using MongoDB.Bson;
+
+namespace Tests.API.Account.Get;
+
+public class Fixture : TestFixture<Program> {
+	public Fixture(IMessageSink sink) : base(sink) { }
+	public readonly string AccountID = ObjectId.GenerateNewId().ToString();
+
+	protected override async Task SetupAsync() {
+		await DB.InsertAsync(new User() {
+			ID = AccountID,
+			DisplayName = ValidAccount.DisplayName,
+			Email = ValidAccount.Email,
+			PasswordHash = BCrypt.Net.BCrypt.HashPassword(ValidAccount.Password),
+			ConversationIDs = new List<string> { "sdfs", "sdfs" },
+			FriendIDs = new List<string> { "sdfs", "sdfs" }
+		});
+
+		await Client.POSTAsync<SigninAPI.Endpoint, SigninAPI.Request>(new SigninAPI.Request() {
+			Email = ValidAccount.Email,
+			Password = ValidAccount.Password,
+		});
+	}
+
+	protected override async Task TearDownAsync() {
+		await DB.DeleteAsync<User>(AccountID);
+	}
+}

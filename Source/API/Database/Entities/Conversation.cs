6079195854e55a8f1d8ea62477a265dc5cad774a
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-11-27 11:05:33 -0800

    modified some properties

--- Source/API/Database/Entities/Conversation.cs
+++ Source/API/Database/Entities/Conversation.cs
@@ -7,10 +7,10 @@ public class Conversation : Entity {
 	public const int MaxNameLength = 25;
 	public const int MinNameLength = 1;
 
-	[BsonId, BsonRepresentation(BsonType.ObjectId)]
-    public string Name { get; set; }
+	[MaxLength(25, ErrorMessage = "Name cannot be more than 25 characters.")]
+	public string Name { get; set; }
 
-	[MaxLength(100, ErrorMessage = "Cannot have more than 100 members.")]
+	[MaxLength(100, ErrorMessage = "A Conversation cannot have more than 100 members")]
 	public List<string> MemberIDs { get; set; } = new List<string>();
 	public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }

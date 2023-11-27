cb2122803f3b0e92fd796e86d74a4fe4e1fc6511
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-11-27 11:50:26 -0800

    finished

--- Source/API/Endpoints/Account/Delete/Endpoint.cs
+++ Source/API/Endpoints/Account/Delete/Endpoint.cs
@@ -8,19 +8,36 @@ sealed class Endpoint : Endpoint<Request> {
         Group<AccountGroup>();
         Version(1);
 
-        Summary(settings => {
-            settings.Summary = "Delete logged in account.";
-        });
+        Summary(settings => settings.Summary = "Delete logged in account.");
     }
 
     public override async Task HandleAsync(Request req, CancellationToken cancellationToken) {
-        var user = await DB.Find<User>().OneAsync(req.AccountID);
+        var account = await DB.Find<User>().OneAsync(req.AccountID);
 
-        if (user == null) {
-            ThrowError("Could not find user.", 404);
+        if (account == null) {
+            ThrowError("Could not find your account.", 404);
         }
 
-        await Data.DeleteUser(user);
-        await CookieAuth.SignOutAsync();
+		var transaction = new Transaction();
+
+		var convo = await transaction.UpdateAndGet<Conversation>()
+			.Match(convo => account.ConversationIDs.Contains(convo.ID))
+			.Modify(convo => convo.Pull(c => c.MemberIDs, account.ID))
+			.ExecuteAsync();
+
+		if (convo.MemberIDs.Count == 0) {
+			await transaction.DeleteAsync<Conversation>(convo.ID);
+		}
+
+		await transaction.Update<User>()
+			.Match(user => account.FriendIDs.Contains(user.ID))
+			.Modify(user => user.Pull(u => u.FriendIDs, account.ID))
+			.ExecuteAsync();
+
+		await transaction.DeleteAsync<User>(account.ID);
+		await transaction.DeleteAsync<FriendRequest>(fr => fr.RequesterID == account.ID);
+
+		await transaction.CommitAsync();
+		await CookieAuth.SignOutAsync();
     }
 }

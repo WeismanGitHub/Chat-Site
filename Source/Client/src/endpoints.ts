2251d1f309d547bb21a4f03d530ec694389e14c0
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-12-05 14:44:08 -0800

    updated amount of retries per request

--- Source/Client/src/endpoints.ts
+++ Source/Client/src/endpoints.ts
@@ -2,15 +2,15 @@ import ky from 'ky';
 
 class Account {
     public static get(): Promise<AccountData> {
-        return ky.get('/API/Account/v1').json();
+        return ky.get('/API/Account/v1', { retry: 0 }).json();
     }
 
     public static signin(data: { email: string; password: string }) {
-        return ky.post('/API/Account/Signin/v1', { json: data }).json();
+        return ky.post('/API/Account/Signin/v1', { json: data, retry: 0 }).json();
     }
 
     public static signup(data: { displayName: string; email: string; password: string }) {
-        return ky.post('/API/Account/Signup/v1', { json: data }).json();
+        return ky.post('/API/Account/Signup/v1', { json: data, retry: 0 }).json();
     }
 
     public static update(data: {
@@ -21,28 +21,29 @@ class Account {
             password: string | null;
         };
     }) {
-        return ky.patch('API/Account/v1', { json: data }).json();
+        return ky.patch('API/Account/v1', { json: data, retry: 0 }).json();
     }
 
     public static delete() {
-        return ky.delete('/API/Account/v1').json();
+        return ky.delete('/API/Account/v1', { retry: 0 }).json();
     }
 
     public static signout() {
-        return ky.post('/API/Account/Signout/v1').json();
+        return ky.post('/API/Account/Signout/v1', { retry: 0 }).json();
     }
 }
 
 class Requests {
     public static get({
         type = 'Incoming',
-        page = 0,
+        page = 1,
     }: {
         type?: 'Incoming' | 'Outgoing';
         page?: number;
     }): Promise<{ friendRequests: FriendRequest[]; totalCount: number }> {
         return ky
             .get('/API/Friends/Requests/v1', {
+                retry: 0,
                 searchParams: {
                     type,
                     page,
@@ -56,25 +57,25 @@ class Friends {
     public static Requests = Requests;
 
     public static get(): Promise<Friend[]> {
-        return ky.get('/API/Friends/v1').json();
+        return ky.get('/API/Friends/v1', { retry: 0 }).json();
     }
 
     public static remove(friendID: string) {
-        return ky.post(`/API/Friends/${friendID}/remove/v1`);
+        return ky.post(`/API/Friends/${friendID}/remove/v1`, { retry: 0 });
     }
 }
 
 class Conversations {
     public static get(): Promise<ConversationsData> {
-        return ky.get('/API/Conversations/v1').json();
+        return ky.get('/API/Conversations/v1', { retry: 0 }).json();
     }
 
     public static leave(conversationID: string) {
-        return ky.post(`/API/Conversations/${conversationID}/leave/v1`);
+        return ky.post(`/API/Conversations/${conversationID}/leave/v1`, { retry: 0 });
     }
 
     public static getOne(conversationID: string): Promise<SingleConvoData> {
-        return ky.get(`/API/Conversations/${conversationID}/v1`).json();
+        return ky.get(`/API/Conversations/${conversationID}/v1`, { retry: 0 }).json();
     }
 }
 

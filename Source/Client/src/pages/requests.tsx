b8ce1a939e57d09a7ef26f4a397c20590e4fd7a3
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-12-05 15:11:10 -0800

    created page

---
+++ Source/Client/src/pages/requests.tsx
@@ -0,0 +1,61 @@
+import { Toast, ToastContainer } from 'react-bootstrap';
+import { useQuery } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
+import Navbar from '../components/navbar';
+import Endpoints from '../endpoints';
+import { HTTPError } from 'ky';
+
+type GetFriendRequesError = {
+    type?: [string];
+    page?: [string];
+};
+
+export default function Requests() {
+    const { data, error } = useQuery<{ friendRequests: FriendRequest[]; totalCount: number }, HTTPError>({
+        queryKey: ['data'],
+        queryFn: () => Endpoints.Friends.Requests.get({}),
+    });
+
+    const [toastError, setToastError] = useState<APIErrorRes<GetFriendRequesError> | null>(null);
+    const [showError, setShowError] = useState(false);
+
+    useEffect(() => {
+        if (error) {
+            error.response.json().then((res) => {
+                console.error(res);
+                setShowError(true);
+                setToastError(res);
+            });
+        }
+    }, [error]);
+
+    return (
+        <div>
+            <Navbar />
+            <ToastContainer position="top-end">
+                <Toast
+                    onClose={() => setShowError(false)}
+                    show={showError}
+                    autohide={true}
+                    className="d-inline-block m-1"
+                    bg={'danger'}
+                >
+                    <Toast.Header>
+                        <strong className="me-auto">
+                            {toastError?.message || 'Unable to read error name.'}
+                        </strong>
+                    </Toast.Header>
+                    <Toast.Body>
+                        {toastError?.errors &&
+                            Object.values(toastError?.errors).map((err) => {
+                                console.log(err);
+                                return <div key={err[0]}>{err[0]}</div>;
+                            })}
+                    </Toast.Body>
+                </Toast>
+            </ToastContainer>
+
+            {data?.totalCount}
+        </div>
+    );
+}

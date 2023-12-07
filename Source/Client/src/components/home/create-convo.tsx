05f2d7784deb6fd6109f4d7e4e857593e52d21a0
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-12-06 16:52:03 -0800

    pass in setConversations

--- Source/Client/src/components/home/create-convo.tsx
+++ Source/Client/src/components/home/create-convo.tsx
@@ -1,3 +1,10 @@
-export default function CreateConvo() {
+import { Dispatch, SetStateAction } from 'react';
+
+export default function CreateConvo({
+    setConversations,
+}: {
+    setConversations: Dispatch<SetStateAction<ConversationsData>>;
+}) {
+    setConversations;
     return <div className="btn btn-outline-primary">Create Convo</div>;
 }
--- Source/Client/src/components/home/join-convo.tsx
+++ Source/Client/src/components/home/join-convo.tsx
@@ -1,3 +1,10 @@
-export default function JoinConvo() {
+import { Dispatch, SetStateAction } from 'react';
+
+export default function JoinConvo({
+    setConversations,
+}: {
+    setConversations: Dispatch<SetStateAction<ConversationsData>>;
+}) {
+    setConversations;
     return <div className="btn btn-outline-primary">Join Convo</div>;
 }

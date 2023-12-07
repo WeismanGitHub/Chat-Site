1f848b9dd2bb336a1c409e35bd02f9fa3d39c2e6
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-12-06 16:52:26 -0800

    toggle friends/conversations

--- Source/Client/src/pages/home.tsx
+++ Source/Client/src/pages/home.tsx
@@ -6,33 +6,54 @@ import AddFriend from '../components/home/add-friend';
 import JoinConvo from '../components/home/join-convo';
 import Friends from '../components/home/friends';
 import Navbar from '../components/navbar';
+import { useState } from 'react';
 
 export default function Home() {
     redirectIfNotLoggedIn();
 
+    const [convoID, setConvoID] = useState<string | null>(null);
+    const [conversations, setConversations] = useState<ConversationsData>([]);
+    const [toggle, setToggle] = useState(true);
+
+    console.log(convoID);
+
     return (
         <div className="overflow-y-hidden vh-100 vw-100">
             <Navbar />
-
-            <div className="col text-center h-100 m-1" style={{ width: '20%' }}>
-                <div className="row justify-content-evenly mb-1">
-                    <div>
-                        <AddFriend />
+            <div className="row vh-100 " style={{ width: '20%' }}>
+                <div className="w-100 text-center">
+                    <div className="btn btn-outline-primary w-50 m-1" onClick={() => setToggle(!toggle)}>
+                        Show {toggle ? 'Friends' : 'Convos'}
                     </div>
                 </div>
-                <div className="overflow-y-scroll h-100">
-                    <Friends />
-                </div>
-            </div>
-            <div className="col text-center h-100" style={{ width: '20%' }}>
-                <div className="row justify-content-evenly m-1">
-                    <div>
-                        <CreateConvo /> <JoinConvo />
+                {toggle ? (
+                    <div className="col text-center h-100 m-1">
+                        <div className="row justify-content-evenly mb-1">
+                            <div>
+                                <CreateConvo setConversations={setConversations} />{' '}
+                                <JoinConvo setConversations={setConversations} />
+                            </div>
+                        </div>
+                        <div className="overflow-y-scroll h-100">
+                            <Conversations
+                                conversations={conversations}
+                                setConvoID={setConvoID}
+                                setConversations={setConversations}
+                            />
+                        </div>
                     </div>
-                </div>
-                <div className="overflow-y-scroll h-100">
-                    <Conversations />
-                </div>
+                ) : (
+                    <div className="col text-center h-100 m-1">
+                        <div className="row justify-content-evenly mb-1">
+                            <div>
+                                <AddFriend />
+                            </div>
+                        </div>
+                        <div className="overflow-y-scroll h-100">
+                            <Friends />
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );

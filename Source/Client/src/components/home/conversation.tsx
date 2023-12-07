2ecf71877fba6d707c2ec4da1435f62dadbbe0b1
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-12-06 16:52:40 -0800

    started work on it

--- Source/Client/src/components/home/conversation.tsx
+++ Source/Client/src/components/home/conversation.tsx
@@ -1,11 +1,17 @@
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
-import { useEffect, useState } from 'react';
 import Endpoints from '../../endpoints';
 import { HTTPError } from 'ky';
 
 type GetOneError = object;
 
-export default function conversation({ conversationID }: { conversationID: string }) {
+export default function conversation({
+    conversationID,
+    setConversations,
+}: {
+    conversationID: string;
+    setConversations: Dispatch<SetStateAction<ConversationsData>>;
+}) {
     const { error, data } = useQuery<SingleConvoData, HTTPError>({
         queryKey: ['data'],
         queryFn: () => Endpoints.Conversations.getOne(conversationID),
@@ -13,9 +19,25 @@ export default function conversation({ conversationID }: { conversationID: strin
 
     const [toastError, setToastError] = useState<APIErrorRes<GetOneError> | null>(null);
     const [showError, setShowError] = useState(false);
+
     data;
     showError;
     toastError;
+    setConversations;
+
+    // async function leaveConvo() {
+    //     try {
+    //         await Endpoints.Conversations.leave(selectedConvo!.id);
+    //         data = data!.filter((convo) => convo.id === selectedConvo?.id);
+    //         setConvo(null);
+    //     } catch (err: unknown) {
+    //         if (err instanceof HTTPError) {
+    //             setToastError(await err.response.json());
+    //             setShowError(true);
+    //             console.log(toastError);
+    //         }
+    //     }
+    // }
 
     useEffect(() => {
         if (error) {
@@ -27,5 +49,5 @@ export default function conversation({ conversationID }: { conversationID: strin
         }
     }, [error]);
 
-    return <>sdfsfs</>;
+    return <>{data?.members}</>;
 }

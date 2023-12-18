b1350e5a220a954dc80bc63d5d39da35be20a5e2
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-12-17 20:25:16 -0800

    got basic layout done

--- Source/Client/src/pages/requests.tsx
+++ Source/Client/src/pages/requests.tsx
@@ -1,9 +1,7 @@
 import { Toast, ToastContainer } from 'react-bootstrap';
-import { useQuery } from '@tanstack/react-query';
 import { useEffect, useState } from 'react';
 import Navbar from '../components/navbar';
 import Endpoints from '../endpoints';
-import { HTTPError } from 'ky';
 
 type GetFriendRequesError = {
     type?: [string];
@@ -11,23 +9,27 @@ type GetFriendRequesError = {
 };
 
 export default function Requests() {
-    const { data, error } = useQuery<{ friendRequests: FriendRequest[]; totalCount: number }, HTTPError>({
-        queryKey: ['data'],
-        queryFn: () => Endpoints.Friends.Requests.get({}),
-    });
-
     const [toastError, setToastError] = useState<APIErrorRes<GetFriendRequesError> | null>(null);
+    const [requests, setRequests] = useState<FriendRequest[]>([]);
+    const [total, setTotal] = useState<number | null>(null);
     const [showError, setShowError] = useState(false);
+    const [type, SetType] = useState<'Incoming' | 'Outgoing'>('Incoming');
+    const [page, setPage] = useState(1);
 
     useEffect(() => {
-        if (error) {
-            error.response.json().then((res) => {
-                console.error(res);
-                setShowError(true);
-                setToastError(res);
+        Endpoints.Friends.Requests.get({ page, type })
+            .then((res) => {
+                setRequests(res?.friendRequests ?? []);
+                setTotal(res?.totalCount ?? null);
+            })
+            .catch((err: APIErrorRes<object>) => {
+                setToastError(err);
             });
-        }
-    }, [error]);
+    }, [type]);
+
+    setPage;
+    page;
+    console.log(total, requests);
 
     return (
         <div>
@@ -54,8 +56,31 @@ export default function Requests() {
                     </Toast.Body>
                 </Toast>
             </ToastContainer>
-
-            {data?.totalCount}
+            <div className="text-center fs-3">
+                <div
+                    className="btn btn-outline-primary w-25 m-1"
+                    onClick={() => SetType(type == 'Incoming' ? 'Outgoing' : 'Incoming')}
+                >
+                    {type}
+                </div>
+                <br />
+                Total: {total}
+                <br />
+                <ul className="list-group w-75 m-auto">
+                    {requests.map((req) => {
+                        return (
+                            <li
+                                key={req.createdAt}
+                                className="list-group-item bg-light-subtle text-primary border-secondary m-2"
+                            >
+                                {req.message}
+                                <br />
+                                {type === 'Outgoing' ? <div className="btn btn-danger">Delete</div> : <div></div>}
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
         </div>
     );
 }

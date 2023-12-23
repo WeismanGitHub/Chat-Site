56c102ad84fa22690c44c2b041d6f0cdfcd893cc
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-12-23 10:06:21 -0800

    got a rudimentary chathub working

--- Source/API/ChatHub.cs
+++ Source/API/ChatHub.cs
@@ -1,31 +1,18 @@
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.Identity.Client;
 
 namespace API;
 public class ChatHub : Hub {
-	private readonly IHttpContextAccessor _httpContextAccessor;
-
-	public ChatHub(IHttpContextAccessor httpContextAccessor) {
-		_httpContextAccessor = httpContextAccessor;
-	}
-
-	private string GetAccountIDFromCookie() {
-		// throw error if accountID is missing? idrk
-		return _httpContextAccessor.HttpContext?.Request.Cookies[Claim.AccountID];
-	}
-
-	public async Task ConnectConvo(string convoID) {
-		var accountID = GetAccountIDFromCookie();
-
+	public async Task JoinConvo(string convoID) {
+		await Clients.Group(convoID).SendAsync("UserJoined", new { UserID = "testid" });
 		await Groups.AddToGroupAsync(Context.ConnectionId, convoID);
-		await Clients.Group(convoID).SendAsync("UserConnected", new { UserID = accountID });
 	}
-	public async Task DisconnectConvo(string convoID) {
-		var accountID = GetAccountIDFromCookie();
 
-		await Clients.Group(convoID).SendAsync("UserDisconnected", new { UserID = accountID });
+	public async Task LeaveConvo(string convoID) {
+		await Clients.Group(convoID).SendAsync("UserLeft", new { UserID = "testid" });
 		await Groups.RemoveFromGroupAsync(Context.ConnectionId, convoID);
 	}
-
+	
 	public async Task SendMessage(string convoID, string message) {
 		await Clients.Group(convoID).SendAsync("ReceiveMessage", new { message });
 	}

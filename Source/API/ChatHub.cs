572bb0f90eb68178793633b15f1cba6351beee92
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-12-25 22:19:55 -0800

    working hub

--- Source/API/ChatHub.cs
+++ Source/API/ChatHub.cs
@@ -2,18 +2,10 @@ using Microsoft.AspNetCore.SignalR;
 using Microsoft.Identity.Client;
 
 namespace API;
-public class ChatHub : Hub {
-	public async Task JoinConvo(string convoID) {
-		await Clients.Group(convoID).SendAsync("UserJoined", new { UserID = "testid" });
-		await Groups.AddToGroupAsync(Context.ConnectionId, convoID);
-	}
 
-	public async Task LeaveConvo(string convoID) {
-		await Clients.Group(convoID).SendAsync("UserLeft", new { UserID = "testid" });
-		await Groups.RemoveFromGroupAsync(Context.ConnectionId, convoID);
-	}
-	
-	public async Task SendMessage(string convoID, string message) {
-		await Clients.Group(convoID).SendAsync("ReceiveMessage", new { message });
+public class ChatHub : Hub {
+	public async Task SendMessage(string message) {
+		await Clients.All.SendAsync("ReceiveMessage", message);
+		// await Clients.Group("convoID").SendAsync("ReceiveMessage", new { message });
 	}
 }

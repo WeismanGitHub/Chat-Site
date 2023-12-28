1b107ed0b2b69bd2c316912fc1388ef0636d22d8
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-12-28 10:16:39 -0800

    completed

--- Source/API/ChatHub.cs
+++ Source/API/ChatHub.cs
@@ -1,39 +1,73 @@
-using API.Database.Entities;
-
 using Microsoft.AspNetCore.SignalR;
+using System.Collections.Concurrent;
+
 namespace API;
 
 public class ChatHub : Hub {
+	private readonly ConcurrentDictionary<string, string> ConversationMap = new ConcurrentDictionary<string, string>();
+
 	public override async Task OnConnectedAsync() {
-		// string conversationID = Context.GetHttpContext()?.Request.Query["conversationID"];
-		// string accountID = Context.GetHttpContext()?.Request.Query["accountID"];
+		try {
+			var httpContext = Context.GetHttpContext();
 
-		// if (conversationID == null || accountID == null) {
-		// 	return;
-		// }
+			if (httpContext == null || httpContext.User?.Identity?.IsAuthenticated == false) {
+				throw new Exception("You must log in.");
+			}
 
-		// var user = await DB.Find<User>().MatchID(accountID).ExecuteSingleAsync();
+			string conversationID = Context.GetHttpContext()?.Request.Query["conversationID"];
+			var accountID = httpContext?.User?.FindFirst("accountID")?.Value;
 
-		// if (user == null || !user.ConversationIDs.Contains(conversationID)) {
-		// 	return;
-		// }
+			if (conversationID == null || accountID == null) {
+				throw new Exception("Missing conversation ID or account ID.");
+			}
 
-		// await Groups.AddToGroupAsync(this.Context.ConnectionId, conversationID);
-		// await Clients.Group(conversationID).SendAsync("UserJoined", accountID);
+			ConversationMap[Context.ConnectionId] = conversationID;
+			var user = await DB.Find<User>().MatchID(accountID).ExecuteSingleAsync();
+
+			if (user == null) {
+				throw new Exception("Could not find user in database.");
+			} else if (!user.ConversationIDs.Contains(conversationID)) {
+				throw new Exception("User not in this conversation.");
+			}
+
+			 await Groups.AddToGroupAsync(this.Context.ConnectionId, conversationID);
+			 await Clients.Group(conversationID).SendAsync("UserJoined", accountID);
+		} catch (Exception ex) {
+			Clients.Caller.SendAsync("ReceiveError", ex.Message);
+		}
 	}
 
 	public override async Task OnDisconnectedAsync(Exception exception) {
-		// var connectionGroups = await Groups.GetGroupsAsync(Context.ConnectionId);
+		try {
+			var accountID = Context.GetHttpContext()?.User?.FindFirst("accountID")?.Value;
+			var conversationID = ConversationMap[Context.ConnectionId];
 
-		// // Handle the groups associated with the connection
-		// foreach (var group in connectionGroups) {
-		// 	// Do something with the group (e.g., log it, send it to the client)
-		// 	await Clients.Caller.SendAsync("ReceiveGroup", group);
-		// }
+			if (conversationID == null || accountID == null) {
+				throw new Exception("Missing conversation ID or account ID.");
+			}
+
+			ConversationMap.Remove(Context.ConnectionId, out conversationID);
+
+			await Groups.RemoveFromGroupAsync(Context.ConnectionId, conversationID);
+			await Clients.Group(conversationID).SendAsync("UserLeft", accountID);
+		} catch (Exception ex) {
+			Clients.Caller.SendAsync("ReceiveError", ex.Message);
+		}
 	}
 
 	public async Task SendMessage(string message) {
-		await Clients.All.SendAsync("ReceiveMessage", message);
-		// await Clients.Group("convoID").SendAsync("ReceiveMessage", new { message });
+		try {
+			var accountID = Context.GetHttpContext()?.User?.FindFirst("accountID")?.Value;
+			var conversationID = ConversationMap[Context.ConnectionId];
+
+			if (conversationID == null || accountID == null) {
+				throw new Exception("Missing conversation ID or account ID.");
+			}
+
+			await Clients.All.SendAsync("ReceiveMessage", message);
+			await Clients.Group("convoID").SendAsync("ReceiveMessage", new { message, accountID });
+		} catch (Exception ex ) {
+			Clients.Caller.SendAsync("ReceiveError", ex.Message);
+		}
 	}
 }

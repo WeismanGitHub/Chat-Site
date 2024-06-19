5366b7174ed0b293da3bd5070a58d66e3d3b323e
weisman <weisman123@outlook.com>
2024-06-18 23:35:09 -0700

    rewrite navbar

--- Source/Client/src/components/navbar.tsx
+++ Source/Client/src/components/navbar.tsx
@@ -1,5 +1,5 @@
-import { ToastContainer, Toast } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import { ToastContainer, Toast, Modal, Button } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
 import Endpoints from '../endpoints';
 import { useState } from 'react';
 import axios from 'axios';
@@ -16,6 +16,7 @@ export default function Navbar() {
     const [showError, setShowError] = useState(false);
     const [error, setError] = useState<APIErrorRes<LogoutError> | null>(null);
     const toggleError = () => setShowError(!showError);
+    const [showModal, setShowModal] = useState(false);
 
     async function logout() {
         await Endpoints.Account.signout()
@@ -52,40 +53,82 @@ export default function Navbar() {
                     </Toast.Body>
                 </Toast>
             </ToastContainer>
-            <nav className="navbar navbar-expand navbar-dark bg-primary ps-2 pe-2 justify-content-center py-1">
-                <a className="navbar-brand" href="/">
-                    <img src="/icon.png" width={50} height={50} alt="icon" className="me-2" />
-                    Chat Site v2
-                </a>
 
-                <div className="justify-content-start navbar-nav">
-                    <a className="nav-item nav-link active" href="/">
-                        Home
-                    </a>
-                    {loggedIn && (
-                        <a className="nav-item nav-link active" href="/account">
-                            Account
-                        </a>
-                    )}
-                    {loggedIn && (
-                        <a className="nav-item nav-link active" href="/requests">
-                            Requests
-                        </a>
-                    )}
-                    <a className="nav-item nav-link active" href="/about">
-                        About
-                    </a>
-                    <div className="ms-10">
-                        {loggedIn ? (
-                            <button className="nav-item nav-link active" onClick={() => logout()}>
-                                Logout
-                            </button>
-                        ) : (
-                            <a className="nav-item nav-link active" href="/auth">
-                                Signup/Signin
-                            </a>
-                        )}
-                    </div>
+            <Modal
+                show={showModal}
+                centered
+                keyboard={true}
+                onHide={() => setShowModal(false)}
+                animation={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Are you sure you want to log out?</Modal.Title>
+                </Modal.Header>
+                <Modal.Footer>
+                    <Button variant="danger" onClick={logout}>
+                        Log Out
+                    </Button>
+                    <Button variant="secondary" onClick={() => setShowModal(false)}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+            <nav className="navbar navbar-expand-lg navbar-expand-md navbar-dark bg-primary fixed-top p-1 ps-3 pe-3" id="navbar">
+                <Link className="navbar-brand m-0" to={'/'}>
+                    <img src="/icon.png" width={50} height={50} alt="icon" className="me-2" />
+                    <span className="fs-3">Chat Site v2</span>
+                </Link>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarResponsive"
+                    aria-controls="navbarResponsive"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarResponsive">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" to={'/'}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to={'/about'}>
+                                About
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to={'/account'}>
+                                Account
+                            </Link>
+                        </li>
+                        {loggedIn && 
+                            <li className="nav-item">
+                            <Link className="nav-link" to={'/requests'}>
+                                Requests
+                            </Link>
+                        </li>}
+                        <li className="nav-item">
+                            {loggedIn ? (
+                                <div
+                                    className="nav-link"
+                                    style={{ cursor: 'pointer' }}
+                                    onClick={() => setShowModal(true)}
+                                >
+                                    Log Out
+                                </div>
+                            ) : (
+                                <Link className="nav-link" to="/Auth">
+                                    Login
+                                </Link>
+                            )}
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </>

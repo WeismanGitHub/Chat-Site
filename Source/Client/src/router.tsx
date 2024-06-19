e75c6eff163b6641afbaf63155d869ee69e9f8d0
weisman <weisman123@outlook.com>
2024-06-19 00:09:48 -0700

    Remove requests path

--- Source/Client/src/router.tsx
+++ Source/Client/src/router.tsx
@@ -1,13 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
 
-import Requests from './pages/requests.tsx';
 import Account from './pages/account.tsx';
 import About from './pages/about.tsx';
 import Auth from './pages/auth.tsx';
 import Home from './pages/home.tsx';
 
 const router = createBrowserRouter([
-    { path: '/Requests', element: <Requests /> },
     { path: '/Account', element: <Account /> },
     { path: '/About', element: <About /> },
     { path: '/Auth', element: <Auth /> },

b6fafde17fcff57f38f228fb0a26acd8d0e35558
weisman <weisman123@outlook.com>
2024-06-19 19:06:45 -0700

    Join with name/password

--- Source/API/Endpoints/ChatRooms/Join.cs
+++ Source/API/Endpoints/ChatRooms/Join.cs
@@ -1,30 +1,35 @@
-﻿namespace API.Endpoints.ChatRooms.SingleChatRoom.Join;
+﻿namespace API.Endpoints.ChatRooms.Join;
 
 public sealed class Request {
 	[From(Claim.AccountID, IsRequired = true)]
 	public string AccountID { get; set; }
-	public required string ChatRoomID { get; set; }
+	public required string Name { get; set; }
+	public required string Password { get; set; }
 
 }
 
 public sealed class Endpoint : Endpoint<Request> {
-    public override void Configure() {
-		Post("/{ChatRoomID}/join");
+	public override void Configure() {
+		Post("/Join");
 		Group<ChatRoomGroup>();
-        Version(1);
+		Version(1);
 
 		Description(builder => builder.Accepts<Request>());
-    }
+	}
 
 	public override async Task HandleAsync(Request req, CancellationToken cancellationToken) {
-		var chat = await DB.Find<ChatRoom>().MatchID(req.ChatRoomID).ExecuteSingleAsync(cancellationToken);
+		var chat = await DB.Find<ChatRoom>()
+			.Match(c => c.Name == req.Name)
+			.ExecuteSingleAsync(cancellationToken);
 
 		if (chat == null) {
-			ThrowError("Could not find chat room", 404);
+			ThrowError("Invalid Credentials", 400);
 		} else if (chat.MemberIDs.Count >= 100) {
 			ThrowError("Chat room is at capacity.", 400);
 		} else if (chat.MemberIDs.Contains(req.AccountID)) {
 			ThrowError("You are already a member of this chat room.", 400);
+		} else if (BCrypt.Net.BCrypt.Verify(req.Password, chat.PasswordHash)) {
+			ThrowError("Invalid Credentials", 400);
 		}
 
 		var transaction = new Transaction();
@@ -34,9 +39,9 @@ public sealed class Endpoint : Endpoint<Request> {
 
 		await transaction.Update<User>()
 			.MatchID(req.AccountID)
-			.Modify(user => user.Push(u => u.ChatRoomIDs, req.ChatRoomID))
+			.Modify(user => user.Push(u => u.ChatRoomIDs, chat.ID))
 			.ExecuteAsync(cancellationToken);
 
-		 await transaction.CommitAsync(cancellationToken);
+		await transaction.CommitAsync(cancellationToken);
 	}
 }

d5123c02268b0c277c2c1060b7298669a62af75a
weisman <weisman123@outlook.com>
2024-06-23 23:57:20 -0700

    Remove react-query code

--- Source/Client/src/main.tsx
+++ Source/Client/src/main.tsx
@@ -1,4 +1,3 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom/client';
@@ -7,12 +6,8 @@ import React from 'react';
 import '../theme.css';
 import './style.css';
 
-const queryClient = new QueryClient();
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <RouterProvider router={router} />
-        </QueryClientProvider>
+        <RouterProvider router={router} />
     </React.StrictMode>
 );

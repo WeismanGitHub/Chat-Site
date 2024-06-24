e1b1fd5d9b2b5a9f6012c6b5de34270137f1bbc2
weisman <weisman123@outlook.com>
2024-06-24 03:06:52 -0700

    Make account button render conditionally

--- Source/Client/src/navbar.tsx
+++ Source/Client/src/navbar.tsx
@@ -109,11 +109,13 @@ export default function Navbar() {
                                 About
                             </Link>
                         </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to={'/account'}>
-                                Account
-                            </Link>
-                        </li>
+                        {authenticated && (
+                            <li className="nav-item">
+                                <Link className="nav-link" to={'/account'}>
+                                    Account
+                                </Link>
+                            </li>
+                        )}
                         <li className="nav-item">
                             {authenticated ? (
                                 <div

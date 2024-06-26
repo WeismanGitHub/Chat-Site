98de6c040d29bf72e88660e74379bf280c2384c4
weisman <weisman123@outlook.com>
2024-06-26 04:28:15 -0700

    Remove password

--- Source/API/Endpoints/ChatRooms/Create.cs
+++ Source/API/Endpoints/ChatRooms/Create.cs
@@ -4,7 +4,6 @@ public sealed class Request {
 	[From(Claim.AccountID, IsRequired = true)]
 	public string AccountID { get; set; }
 	public required string Name { get; set; }
-	public required string Password { get; set; }
 }
 
 public sealed class Response {

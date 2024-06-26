ff47e26fe7da14515e1f9f0f81d8b930949bde8d
weisman <weisman123@outlook.com>
2024-06-26 04:25:58 -0700

    Change schema

--- Source/API/Database/Entities/ChatRoom.cs
+++ Source/API/Database/Entities/ChatRoom.cs
@@ -6,12 +6,9 @@ namespace API.Database.Entities;
 public class ChatRoom : Entity {
 	public const int MaxNameLength = 25;
 	public const int MinNameLength = 1;
-	public const int MaxPasswordLength = 70;
-	public const int MinPasswordLength = 10;
 
 	[MaxLength(25, ErrorMessage = "Name cannot be more than 25 characters.")]
 	public required string Name { get; set; }
-	public required string PasswordHash { get; set; }
 
 	[MaxLength(100, ErrorMessage = "A chat room cannot have more than 100 members")]
 	public List<string> MemberIDs { get; set; } = new List<string>();
